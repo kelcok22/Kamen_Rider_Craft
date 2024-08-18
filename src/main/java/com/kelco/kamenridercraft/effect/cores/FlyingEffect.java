72c69a3fbda6141ec41b4a4f93e9cc566ca240d4
kelcok22 <12749084+kelcok22@users.noreply.github.com>
2024-08-18 03:37:00 +0100

    more work on Effects

--- src/main/java/com/kelco/kamenridercraft/effect/cores/FlyingEffect.java
+++ src/main/java/com/kelco/kamenridercraft/effect/cores/FlyingEffect.java
@@ -1,8 +1,11 @@
 package com.kelco.kamenridercraft.effect.cores;
 
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
 
 
 public class FlyingEffect extends MobEffect {
@@ -12,4 +15,15 @@ public class FlyingEffect extends MobEffect {
 		super(mobEffectCategory, color);
 	}
 
+	@Override
+	public boolean shouldApplyEffectTickThisTick(int tickCount, int amplifier) {
+		return true;
+	}
+
+	@Override
+	public boolean applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
+
+		
+		return true;
+	}
 }
