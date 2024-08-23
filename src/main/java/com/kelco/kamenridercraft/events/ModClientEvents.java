48f7ad36ad43a25f372b784df15dffa8e0cde52f
kelcok22 <12749084+kelcok22@users.noreply.github.com>
2024-08-23 17:14:11 +0100

    editing events

---
+++ src/main/java/com/kelco/kamenridercraft/events/ModClientEvents.java
@@ -0,0 +1,52 @@
+package com.kelco.kamenridercraft.events;
+
+import com.kelco.kamenridercraft.effect.Effect_core;
+import com.kelco.kamenridercraft.item.BaseItems.RiderDriverItem;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.client.event.RenderLivingEvent;
+
+public class ModClientEvents {
+
+
+
+
+    public static class ClientEvents {
+
+        @SubscribeEvent
+        public void addRenderLivingEvent(RenderLivingEvent.Pre event) {
+
+            if (event.getEntity().getItemBySlot(EquipmentSlot.FEET).getItem() instanceof RiderDriverItem belt) {
+
+
+                if (event.getEntity().getItemBySlot(EquipmentSlot.HEAD).getItem() == belt.HEAD) {
+                    if (event.getEntity().getItemBySlot(EquipmentSlot.CHEST).getItem() == belt.TORSO) {
+                        if (event.getEntity().getItemBySlot(EquipmentSlot.LEGS).getItem() == belt.LEGS) {
+                            if (RiderDriverItem.get_Form_Item(event.getEntity().getItemBySlot(EquipmentSlot.FEET), 1).get_PalyerModelInvisible())
+                                event.getEntity().setInvisible(true);
+                        }
+                    }
+                }
+            }
+
+            float size = 1;
+            boolean Tall = event.getEntity().hasEffect(Effect_core.STRETCH);
+
+            if (event.getEntity().hasEffect(Effect_core.STRETCH)) {
+                size = size + ((event.getEntity().getEffect(Effect_core.STRETCH).getAmplifier()) + 1);
+            }
+
+            float size2 = event.getEntity().hasEffect(Effect_core.STRETCH) ? 1 : size;
+
+            if (event.getEntity().hasEffect(Effect_core.FLAT)) {
+              size2 = 0.1f;
+            }
+            float size3 = event.getEntity().hasEffect(Effect_core.STRETCH) ? 1 : size;
+            if (event.getEntity().hasEffect(Effect_core.WIDE)) {
+                size2 = (float) (size2 * 3);
+                size3 = (float) (size3 * 3);
+            }
+            event.getPoseStack().scale(size3, size, size2);
+        }
+    }
+}
