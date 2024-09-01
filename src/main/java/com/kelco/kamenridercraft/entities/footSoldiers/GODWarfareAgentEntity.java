c5a6efa613407de6bd47aeb07daa3462c192566e
kelcok22 <12749084+kelcok22@users.noreply.github.com>
2024-09-01 22:56:18 +0100

    edit to some mobs

--- src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/GODWarfareAgentEntity.java
+++ src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/GODWarfareAgentEntity.java
@@ -1,27 +1,12 @@
 package com.kelco.kamenridercraft.entities.footSoldiers;
 
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.level.Level;
 
 public class GODWarfareAgentEntity extends BaseHenchmenEntity {
 	
     public GODWarfareAgentEntity(EntityType<? extends BaseHenchmenEntity> type, Level level) {
         super(type, level);
-        NAME="god_warfare_agents";
+        NAME = "god_warfare_agents";
     }
-
-    public static AttributeSupplier.Builder setAttributes() {
-
-        return Monster.createMonsterAttributes()
-                .add(Attributes.FOLLOW_RANGE, 35.0D)
-                .add(Attributes.MOVEMENT_SPEED,(double)0.23F)
-                .add(Attributes.ATTACK_DAMAGE, 4.0D)
-                .add(Attributes.ARMOR, 3.0D)
-                .add(Attributes.MAX_HEALTH, 30.0D);
-    }
-
 }
--- src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/ShockerCombatmanEntity.java
+++ src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/ShockerCombatmanEntity.java
@@ -5,12 +5,7 @@ import net.minecraft.network.chat.Component;
 import com.kelco.kamenridercraft.entities.MobsCore;
 
 import net.minecraft.ChatFormatting;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
@@ -37,17 +32,4 @@ public class ShockerCombatmanEntity extends BaseHenchmenEntity {
 		}
 		super.remove(reason);
 	}
-
-	public static AttributeSupplier.Builder setAttributes() {
-
-		return Monster.createMonsterAttributes()
-				.add(Attributes.FOLLOW_RANGE, 35.0D)
-				.add(Attributes.MOVEMENT_SPEED,(double)0.23F)
-				.add(Attributes.ATTACK_DAMAGE, 4.0D)
-				.add(Attributes.ARMOR, 3.0D)
-				.add(Attributes.MAX_HEALTH, 30.0D)
-				.add(Attributes.SPAWN_REINFORCEMENTS_CHANCE);
-	}
-
-
 }
