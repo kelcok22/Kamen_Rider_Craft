81b27a88599a581ff1bce811aa4624adee3b55c8
kelcok22 <kelcok22@live.co.uk>
2024-09-01 22:56:18 +0100

    edit to some mobs

--- src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/CombatRoidEntity.java
+++ src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/CombatRoidEntity.java
@@ -1,10 +1,6 @@
 package com.kelco.kamenridercraft.entities.footSoldiers;
 
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.level.Level;
 
 public class CombatRoidEntity extends BaseHenchmenEntity {
@@ -14,13 +10,4 @@ public class CombatRoidEntity extends BaseHenchmenEntity {
         NAME="combatroid";
     }
 
-    public static AttributeSupplier.Builder setAttributes() {
-
-        return Monster.createMonsterAttributes()
-                .add(Attributes.FOLLOW_RANGE, 35.0D)
-                .add(Attributes.MOVEMENT_SPEED,(double)0.23F)
-                .add(Attributes.ATTACK_DAMAGE, 4.0D)
-                .add(Attributes.ARMOR, 3.0D)
-                .add(Attributes.MAX_HEALTH, 30.0D);
-    }
 }
--- src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/ZuGumunBaEntity.java
+++ src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/ZuGumunBaEntity.java
@@ -1,10 +1,6 @@
 package com.kelco.kamenridercraft.entities.footSoldiers;
 
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.level.Level;
 
 public class ZuGumunBaEntity extends BaseHenchmenEntity {
@@ -12,16 +8,5 @@ public class ZuGumunBaEntity extends BaseHenchmenEntity {
     public ZuGumunBaEntity(EntityType<? extends BaseHenchmenEntity> type, Level level) {
         super(type, level);
         NAME="zu_gumun_ba";
-    }
-
-
-    public static AttributeSupplier.Builder setAttributes() {
-
-        return Monster.createMonsterAttributes()
-                .add(Attributes.FOLLOW_RANGE, 35.0D)
-                .add(Attributes.MOVEMENT_SPEED,(double)0.23F)
-                .add(Attributes.ATTACK_DAMAGE, 4.0D)
-                .add(Attributes.ARMOR, 3.0D)
-                .add(Attributes.MAX_HEALTH, 30.0D);
     }
 }
