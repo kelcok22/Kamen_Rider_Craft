c7bfae3329f370169a99cdbf7e671cbfc4060608
kelcok22 <kelcok22@live.co.uk>
2024-09-09 01:17:58 +0100

    I added a lot wizard things

--- src/main/java/com/kelco/kamenridercraft/item/Wizard_Rider_Items.java
+++ src/main/java/com/kelco/kamenridercraft/item/Wizard_Rider_Items.java
@@ -19,6 +19,33 @@ public class Wizard_Rider_Items {
 	public static final DeferredItem<Item> WIZARD_LOGO = ITEMS.register("wizard_logo",
 			() -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
 
+	public static final DeferredItem<Item> WIZARD_GEM= ITEMS.register("wizardgem",
+			() -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> WIZARD_GEM_RED= ITEMS.register("wizardgem_red",
+			() -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> WIZARD_GEM_BLUE= ITEMS.register("wizardgem_blue",
+			() -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> WIZARD_GEM_GREEN= ITEMS.register("wizardgem_green",
+			() -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> WIZARD_GEM_YELLOW= ITEMS.register("wizardgem_yellow",
+			() -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> WIZARD_GEM_VIOLET= ITEMS.register("wizardgem_violet",
+			() -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> WIZARD_GEM_BLACK= ITEMS.register("wizardgem_black",
+			() -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> WIZARD_GEM_CYAN= ITEMS.register("wizardgem_cyan",
+			() -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> PHILOSOPHERS_STONE= ITEMS.register("philosophers_stone",
+			() -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
 	public static final DeferredItem<Item> FLAME_WIZARD_RING = ITEMS.register("flame_ring",
 			() -> new RiderFormChangeItem(new Item.Properties(),0,"","wizard","wizardriver_belt",
 					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
@@ -54,10 +81,17 @@ public class Wizard_Rider_Items {
 					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
 					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
 
+	public static final DeferredItem<Item> LAND_DRAGON_WIZARD_RING_BEAST = ITEMS.register("land_ring_dragon_beast",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_wizard","beast","beast_driver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.ChangeModel("geo/wizard_all_dragon.geo.json"));
+
 	public static final DeferredItem<Item> LAND_DRAGON_WIZARD_RING = ITEMS.register("land_ring_dragon",
 			() -> new RiderFormChangeItem(new Item.Properties(),0,"_land_dragon","wizard","wizardriver_belt",
 					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
-					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.addAlternative(LAND_DRAGON_WIZARD_RING_BEAST.get()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
 
 	public static final DeferredItem<Item> INFINITY_WIZARD_RING = ITEMS.register("infinity_ring",
 			() -> new RiderFormChangeItem(new Item.Properties(),0,"_infinity","wizard","wizardriver_belt",
@@ -71,26 +105,134 @@ public class Wizard_Rider_Items {
 					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
 					.addNeedForm(FLAME_DRAGON_WIZARD_RING.get(),1).ChangeModel("geo/wizard_all_dragon.geo.json").AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
 
+	public static final DeferredItem<Item> WATER_DRAGON_WIZARD_SPECIAL_RING = ITEMS.register("water_ring_dragon_special_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_water_dragon_special","wizard","wizardriver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.addNeedForm(WATER_DRAGON_WIZARD_RING.get(),1).ChangeModel("geo/wizard_all_dragon.geo.json"));
+
+	public static final DeferredItem<Item> HURRICANE_DRAGON_WIZARD_SPECIAL_RING = ITEMS.register("hurricane_ring_dragon_special_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_hurricane_dragon_special","wizard","wizardriver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.addNeedForm(HURRICANE_DRAGON_WIZARD_RING.get(),1).addAlternative(WATER_DRAGON_WIZARD_SPECIAL_RING.get()).ChangeModel("geo/wizard_all_dragon.geo.json"));
+
+	public static final DeferredItem<Item> LAND_DRAGON_WIZARD_SPECIAL_RING = ITEMS.register("land_ring_dragon_special_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_land_dragon_special","wizard","wizardriver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.addNeedForm(LAND_DRAGON_WIZARD_RING.get(),1).addAlternative(HURRICANE_DRAGON_WIZARD_SPECIAL_RING.get()).ChangeModel("geo/wizard_all_dragon.geo.json"));
+
+	public static final DeferredItem<Item> SPECIAL_RING = ITEMS.register("special_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_flame_dragon_special","wizard","wizardriver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.ChangeModel("geo/wizard_all_dragon.geo.json").addAlternative(LAND_DRAGON_WIZARD_SPECIAL_RING.get())
+					.addNeedForm(FLAME_DRAGON_WIZARD_RING.get(),1).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> HOPE_RING = ITEMS.register("hope_ring",
+			() -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> FINISH_STRIKE_RING_NO_HOPE  = ITEMS.register("finish_strike_ring_no_hope",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_infinity_dragon","wizard","wizardriver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.addNeedForm(INFINITY_WIZARD_RING.get(),1).ChangeModel("geo/wizard_all_dragon.geo.json"));
+
+	public static final DeferredItem<Item> FINISH_STRIKE_RING  = ITEMS.register("finish_strike_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_infinity_gold","wizard","wizardriver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.addNeedForm(INFINITY_WIZARD_RING.get(),1).ChangeModel("geo/wizard_all_dragon.geo.json")
+					.addNeedItem(HOPE_RING.get()).addAlternative(FINISH_STRIKE_RING_NO_HOPE.get()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> SPECIAL_RUSH_RING = ITEMS.register("special_rush_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_flame_dragon_special_rush","wizard","wizardriver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.addNeedForm(FLAME_DRAGON_WIZARD_RING.get(),1).ChangeModel("geo/wizard_all_dragon.geo.json").AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> BEAST_RING = ITEMS.register("beast_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"","beast","beast_driver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> FALCO_RING_WIZARD = ITEMS.register("falco_ring_wizard",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_falco","wizard","wizardriver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.addNeedForm(FLAME_DRAGON_WIZARD_RING.get(),1));
+
+	public static final DeferredItem<Item> FALCO_RING = ITEMS.register("falco_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_falco","beast","beast_driver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.addAlternative(FALCO_RING_WIZARD.get()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> CHAMELEO_RING = ITEMS.register("chameleo_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_chameleo","beast","beast_driver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+
+	public static final DeferredItem<Item> BUFFA_RING_WIZARD = ITEMS.register("buffa_ring_wizard",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_beast","wizard","wizardriver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.addNeedForm(FALCO_RING_WIZARD.get(),1));
+
+	public static final DeferredItem<Item> BUFFA_RING = ITEMS.register("buffa_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_buffa","beast","beast_driver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.addAlternative(BUFFA_RING_WIZARD.get()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> DOLPHI_RING = ITEMS.register("dolphi_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_dolphi","beast","beast_driver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> HYPER_RING = ITEMS.register("hyper_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_hyper","beast","beast_driver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> WISEMAN_RING = ITEMS.register("wiseman_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"","wiseman","wise_driver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> MAGE_O_RING = ITEMS.register("mage_o_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"","mage","wise_driver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> MAGE_B_RING = ITEMS.register("mage_b_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"","mage_blue","wise_driver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> MAGE_G_RING = ITEMS.register("mage_g_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"","mage_green","wise_driver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> SORCERER_RING = ITEMS.register("sorcerer_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"","sorcerer","wise_driver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> BLACK_WIZARD_RING = ITEMS.register("black_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"","black_wizard","wizardriver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> DARK_WIZARD_RING = ITEMS.register("dark_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"","dark_wizard","wizardriver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false)).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
 	/*
-	special_ring
-	finish_strike_ring
-	hope_ring
-	special_rush_ring
-
-	beast_ring
-	falco_ring
-	chameleo_ring
-	buffa_ring
-	dolphi_ring
-	hyper_ring
-
-	wiseman_ring
-	mage_o_ring
-	mage_b_ring
-	mage_g_ring
-	sorcerer_ring
-	black_ring
-	dark_ring
 
 	light_ring
 	excite_ring
@@ -108,8 +250,15 @@ public class Wizard_Rider_Items {
 	drill_ring
 	big_ring
 	small_ring
-	dress_up_ring
+	*/
+	public static final DeferredItem<Item> DRESS_UP_RING = ITEMS.register("dress_up_ring",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_dressup","wizard","wizardriver_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 0,true,false)
+					,new MobEffectInstance(MobEffects.JUMP, 400, 1,true,false))
+					.addNeedForm(FLAME_WIZARD_RING.get(),1).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
 
+
+/*
 	engage_ring
 	please_ring
 	driver_on_ring
@@ -146,6 +295,62 @@ public class Wizard_Rider_Items {
 			() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"wizard",FLAME_WIZARD_RING , WIZARD_LOGO_HELMET, WIZARD_LOGO_CHESTPLATE, WIZARD_LOGO_LEGGINGS, new Item.Properties())
 			.AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
 
+	public static final DeferredItem<Item> BEAST_DRIVER = ITEMS.register("beastdriver",
+			() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"beast",BEAST_RING , WIZARD_LOGO_HELMET, WIZARD_LOGO_CHESTPLATE, WIZARD_LOGO_LEGGINGS, new Item.Properties())
+					.AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> WHITE_WIZARD_DRIVER = ITEMS.register("whitewizardriver",
+			() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"wiseman",WISEMAN_RING , WIZARD_LOGO_HELMET, WIZARD_LOGO_CHESTPLATE, WIZARD_LOGO_LEGGINGS, new Item.Properties())
+					.AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> MAGE_DRIVER = ITEMS.register("magewizardriver",
+			() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"mage",MAGE_O_RING , WIZARD_LOGO_HELMET, WIZARD_LOGO_CHESTPLATE, WIZARD_LOGO_LEGGINGS, new Item.Properties())
+					.AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> MAGE_DRIVER_B = ITEMS.register("magewizardriver_b",
+			() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"mage_blue",MAGE_B_RING , WIZARD_LOGO_HELMET, WIZARD_LOGO_CHESTPLATE, WIZARD_LOGO_LEGGINGS, new Item.Properties())
+					.AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> MAGE_DRIVER_G = ITEMS.register("magewizardriver_g",
+			() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"mage_green",MAGE_G_RING , WIZARD_LOGO_HELMET, WIZARD_LOGO_CHESTPLATE, WIZARD_LOGO_LEGGINGS, new Item.Properties())
+					.AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> MAGE_DRIVER_FOOT_SOLDIERS = ITEMS.register("magewizardriver_foot_soldiers",
+			() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"mage_foot_soldiers",MAGE_O_RING , WIZARD_LOGO_HELMET, WIZARD_LOGO_CHESTPLATE, WIZARD_LOGO_LEGGINGS, new Item.Properties())
+					.AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> MAGE_DRIVER_CAPTAIN = ITEMS.register("magewizardriver_captain",
+			() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"mage_captain",MAGE_O_RING , WIZARD_LOGO_HELMET, WIZARD_LOGO_CHESTPLATE, WIZARD_LOGO_LEGGINGS, new Item.Properties())
+					.AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> SORCERER_DRIVER = ITEMS.register("sorcererdriver",
+			() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"sorcerer",SORCERER_RING , WIZARD_LOGO_HELMET, WIZARD_LOGO_CHESTPLATE, WIZARD_LOGO_LEGGINGS, new Item.Properties())
+					.AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> WHITE_WIZARD_DRIVER_F = ITEMS.register("whitewizardriver_f",
+			() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"wiseman_female",WISEMAN_RING , WIZARD_LOGO_HELMET, WIZARD_LOGO_CHESTPLATE, WIZARD_LOGO_LEGGINGS, new Item.Properties())
+					.AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> DARK_WIZARDRIVER = ITEMS.register("dark_wizardriver",
+			() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"dark_wizard",DARK_WIZARD_RING , WIZARD_LOGO_HELMET, WIZARD_LOGO_CHESTPLATE, WIZARD_LOGO_LEGGINGS, new Item.Properties())
+					.AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> BLACK_WIZARDRIVER = ITEMS.register("black_wizardriver",
+			() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"black_wizard",BLACK_WIZARD_RING , WIZARD_LOGO_HELMET, WIZARD_LOGO_CHESTPLATE, WIZARD_LOGO_LEGGINGS, new Item.Properties())
+					.AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+	public static final DeferredItem<Item> WIZARD_GEM_CRAFTING_CHISEL= ITEMS.register("wizard_gem_crafting_chisel",
+			() -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.WIZARD_TAB_ITEM));
+
+/*
+	wizarswordgun
+	axcalibur
+	wizarswordgun_mage
+	hammelcane
+	dis_halberd
+	mirage_magnum
+	dice_saber
+*/
 
 	public static void register(IEventBus eventBus) {ITEMS.register(eventBus);}
 }
