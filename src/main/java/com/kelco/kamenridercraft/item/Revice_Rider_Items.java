97398457478df24ec7fb1bd6931420c1363fe10f
Lovekov <168786855+LovekovGaming@users.noreply.github.com>
2024-09-27 14:56:10 -0700

    Correction

--- src/main/java/com/kelco/kamenridercraft/item/Revice_Rider_Items.java
+++ src/main/java/com/kelco/kamenridercraft/item/Revice_Rider_Items.java
@@ -90,17 +90,25 @@ public class Revice_Rider_Items {
 
 	public static final DeferredItem<Item> GIFFARD_REX_VISTAMP_VICE = ITEMS.register("giffard_rex_vistamp_vice",
         () -> new RiderFormChangeItem(new Item.Properties(),0,"_ultimate","vice","revice_driver_belt_g",
-                new MobEffectInstance(MobEffects.JUMP, 40, 5,true,false),
-                new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
-                new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false),
-                new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 1,true,false)));
+				new MobEffectInstance(MobEffects.JUMP, 40, 6,true,false),
+				new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 3,true,false),
+				new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 4,true,false),
+				new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 5,true,false),
+				new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 40, 0,true,false),
+				new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0,true,false),
+				new MobEffectInstance(MobEffects.DIG_SPEED, 40, 3,true,false),
+				new MobEffectInstance(Effect_core.PUNCH, 40, 8,true,false)));
 
 	public static final DeferredItem<Item> GIFFARD_REX_VISTAMP = ITEMS.register("giffard_rex_vistamp",
         () -> new RiderFormChangeItem(new Item.Properties(),0,"_ultimate","revi","revice_driver_belt_g",
-                new MobEffectInstance(MobEffects.JUMP, 40, 5,true,false),
-                new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
-                new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false),
-                new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 1,true,false)).addAlternative(GIFFARD_REX_VISTAMP_VICE.get()).AddToList(RiderTabs.REVICE_TAB_ITEM));
+				new MobEffectInstance(MobEffects.JUMP, 40, 6,true,false),
+				new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 3,true,false),
+				new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 4,true,false),
+				new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 5,true,false),
+				new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 40, 0,true,false),
+				new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0,true,false),
+				new MobEffectInstance(MobEffects.DIG_SPEED, 40, 3,true,false),
+				new MobEffectInstance(Effect_core.PUNCH, 40, 8,true,false)).addAlternative(GIFFARD_REX_VISTAMP_VICE.get()).AddToList(RiderTabs.REVICE_TAB_ITEM));
 
 	public static final DeferredItem<Item> FIFTY_GALE_VISTAMP = ITEMS.register("fifty_gale_vistamp",
         () -> new RiderFormChangeItem(new Item.Properties(),0,"_igarashi","revi","revice_driver_belt_f",
