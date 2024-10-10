67ff8d30ac7093f41a133ffc6ec20761b7b9e444
kelcok22 <kelcok22@live.co.uk>
2024-10-10 18:11:44 +0100

    work on Vistamp Bar

--- src/main/java/com/kelco/kamenridercraft/block/machineBlocks/VistampBar.java
+++ src/main/java/com/kelco/kamenridercraft/block/machineBlocks/VistampBar.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Random;
 
 
+import com.kelco.kamenridercraft.item.Ex_Aid_Rider_Items;
 import com.kelco.kamenridercraft.item.Revice_Rider_Items;
 
+import com.kelco.kamenridercraft.item.Zero_One_Rider_Items;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
@@ -65,12 +67,26 @@ public class VistampBar extends MachineBlock {
         return PushReaction.PUSH_ONLY;
      }
 
-     private Item getVistampDrop(int num) {
+     private Item getgashatDrop(Player player) {
  		Random generator = new Random();
- 		//if (num==0){
- 			int rand = generator.nextInt(PROTO_VISTAMP.size());
- 			return PROTO_VISTAMP.get(rand);
- 		//}
+
+         List<Item> PROTO_VISTAMP_PLUS= new ArrayList<Item>();
+         PROTO_VISTAMP_PLUS.clear();
+         PROTO_VISTAMP_PLUS.addAll (PROTO_VISTAMP);
+
+         if (player.getInventory().countItem(Revice_Rider_Items.BARID_REX_VISTAMP.get())!=0){
+             for (int i = 0; i < 20; i++) {
+                 PROTO_VISTAMP_PLUS.add(Revice_Rider_Items.VOLCANO_VISTAMP.get());
+             }
+         }
+         if (player.getInventory().countItem(Revice_Rider_Items.ROLLING_VISTAMP.get())!=0){
+             for (int i = 0; i < 20; i++) {
+                 PROTO_VISTAMP_PLUS.add(Revice_Rider_Items.THUNDER_GALE_VISTAMP.get());
+             }
+         }
+ 			int rand = generator.nextInt(PROTO_VISTAMP_PLUS.size());
+ 			return PROTO_VISTAMP_PLUS.get(rand);
+
  	}
 
     @Override
@@ -78,14 +94,23 @@ public class VistampBar extends MachineBlock {
 
         if (!level.isClientSide()) {
             if (player.getItemInHand(hand).getItem() == Revice_Rider_Items.PROTO_VISTAMP.get()){
-                if (player.getInventory().countItem(Revice_Rider_Items.BARID_REX_VISTAMP.get())!=0){
-                    for (int i = 0; i < 4; i++) PROTO_VISTAMP.add(Revice_Rider_Items.VOLCANO_VISTAMP.get());
-                }
-                if (player.getInventory().countItem(Revice_Rider_Items.ROLLING_VISTAMP.get())!=0){
-                    for (int i = 0; i < 4; i++)PROTO_VISTAMP.add(Revice_Rider_Items.THUNDER_GALE_VISTAMP.get());
-                }
-                process(player, level, pos, hand, getVistampDrop(0));
-                PROTO_VISTAMP.removeIf(item -> item == Revice_Rider_Items.VOLCANO_VISTAMP.get() || item == Revice_Rider_Items.THUNDER_GALE_VISTAMP.get());
+                process(player, level, pos, hand, getgashatDrop( player));
+                return ItemInteractionResult.SUCCESS;
+            }
+            else if (player.getItemInHand(hand).getItem() == Revice_Rider_Items.PROTO_QUEEN_BEE_VISTAMP.get()) {
+                process(player, level, pos, hand,  Revice_Rider_Items.QUEEN_BEE_VISTAMP.get());
+                return ItemInteractionResult.SUCCESS;
+            }
+            else if (player.getItemInHand(hand).getItem() == Revice_Rider_Items.PROTO_PLANARIAN_VISTAMP.get()) {
+                process(player, level, pos, hand,  Revice_Rider_Items.PLANARIAN_VISTAMP.get());
+                return ItemInteractionResult.SUCCESS;
+            }
+            else if (player.getItemInHand(hand).getItem() == Revice_Rider_Items.PROTO_CHAMELEON_VISTAMP.get()) {
+                process(player, level, pos, hand,  Revice_Rider_Items.CHAMELEON_VISTAMP.get());
+                return ItemInteractionResult.SUCCESS;
+            }
+            else if (player.getItemInHand(hand).getItem() == Revice_Rider_Items.DAIOUIKA_VISTAMP.get()) {
+                process(player, level, pos, hand,  Revice_Rider_Items.KRAKEN_VISTAMP.get());
                 return ItemInteractionResult.SUCCESS;
             }
         }
