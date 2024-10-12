22c9fbd4bf3747517d3568c48dd42c030a14149a
kelcok22 <kelcok22@live.co.uk>
2024-10-12 18:35:03 +0100

    some fixes

--- src/main/java/com/kelco/kamenridercraft/effect/cores/GreeedEffect.java
+++ src/main/java/com/kelco/kamenridercraft/effect/cores/GreeedEffect.java
@@ -4,9 +4,12 @@ import java.util.Random;
 
 import com.kelco.kamenridercraft.entities.MobsCore;
 import com.kelco.kamenridercraft.entities.footSoldiers.BaseHenchmenEntity;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 
 
 public class GreeedEffect extends MobEffect {
@@ -14,6 +17,11 @@ public class GreeedEffect extends MobEffect {
 
 	public GreeedEffect(MobEffectCategory mobEffectCategory, int color) {
 		super(mobEffectCategory, color);
+	}
+
+	@Override
+	public boolean shouldApplyEffectTickThisTick(int tickCount, int amplifier) {
+		return true;
 	}
 
 	@Override
@@ -28,10 +36,9 @@ public class GreeedEffect extends MobEffect {
 				boss.moveTo(pLivingEntity.getX()+(rand.nextInt(8)-4), pLivingEntity.getY(), pLivingEntity.getZ()+(rand.nextInt(8)-4),0.0f, 0.0F);
 				pLivingEntity.level().addFreshEntity(boss);
 				break;
-
 			}
 		}
-		return false;
+		return true;
 	}
 
 }
