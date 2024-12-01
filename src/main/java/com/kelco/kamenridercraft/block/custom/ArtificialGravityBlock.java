b73bec2423677798259eebccc956e3f7003ce92c
kelcok22 <kelcok22@live.co.uk>
2024-12-01 21:38:06 +0000

    Boonboomger Gochizo

---
+++ src/main/java/com/kelco/kamenridercraft/block/custom/ArtificialGravityBlock.java
@@ -0,0 +1,73 @@
+package com.kelco.kamenridercraft.block.custom;
+
+
+import com.kelco.kamenridercraft.block.baseBlocks.BaseBlock;
+import com.kelco.kamenridercraft.effect.Effect_core;
+import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RedstoneLampBlock;
+import net.minecraft.world.level.block.RedstoneTorchBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.AABB;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class ArtificialGravityBlock extends BaseBlock {
+
+	public ArtificialGravityBlock(Properties prop) {
+		super(prop);
+		
+	}
+
+
+
+	@Override
+	protected BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos pos, BlockPos facingPos) {
+		if (level.hasNeighborSignal(pos)) {
+			level.scheduleTick(pos, this, 20);
+		}
+
+		return super.updateShape(state, facing, facingState, level, pos, facingPos);
+	}
+
+	@Override
+	protected void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean isMoving) {
+		level.scheduleTick(pos, this, 20);
+	}
+
+
+	@Override
+	protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+		if (level.hasNeighborSignal(pos)) {
+			level.scheduleTick(pos, this, 20);
+
+			if (!level.isClientSide()) {
+
+				AABB Effect_pos = new AABB(pos).inflate(15);
+
+				List<LivingEntity> nearbyEntities = level.getEntitiesOfClass(LivingEntity.class, Effect_pos, entity ->
+						(entity instanceof Player)
+								|| (entity instanceof Mob mob));
+				for (LivingEntity enemy : nearbyEntities) {
+					enemy.addEffect(new MobEffectInstance(Effect_core.GRAVITY, 70, 5, true, true));
+				}
+			}
+		}
+	}
+}
