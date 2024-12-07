2b950d98e492538f6f61c3ede9a484660c34d424
kelcok22 <kelcok22@live.co.uk>
2024-12-07 18:16:01 +0000

    gate

--- src/main/java/com/kelco/kamenridercraft/effect/Effect_core.java
+++ src/main/java/com/kelco/kamenridercraft/effect/Effect_core.java
@@ -56,10 +56,6 @@ public class Effect_core {
             () -> new BasicEffect(MobEffectCategory.NEUTRAL, 0x9d3443)
                     .addAttributeModifier(Attributes.STEP_HEIGHT, ResourceLocation.fromNamespaceAndPath(KamenRiderCraftCore.MOD_ID, "effect.step"), 1F, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
 
-    public static final Holder<MobEffect> HEAVY_SWING= EFFECT.register("heavy_swing",
-            () -> new BasicEffect(MobEffectCategory.NEUTRAL, 0xffc646)
-                    .addAttributeModifier(Attributes.SWEEPING_DAMAGE_RATIO, ResourceLocation.fromNamespaceAndPath(KamenRiderCraftCore.MOD_ID, "effect.heavy_swing"), 2F, AttributeModifier.Operation.ADD_MULTIPLIED_BASE));
-
 
     public static final Holder<MobEffect> GRAVITY= EFFECT.register("gravity",
             () -> new BasicEffect(MobEffectCategory.NEUTRAL, 0x1d8519)
