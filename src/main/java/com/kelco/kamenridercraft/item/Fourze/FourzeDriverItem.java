421505ef3db871b54e7fc3e7365fa5080a873f2d
kelcok22 <kelcok22@live.co.uk>
2024-12-09 00:13:46 +0000

    Fusion On!

--- src/main/java/com/kelco/kamenridercraft/item/Fourze/FourzeDriverItem.java
+++ src/main/java/com/kelco/kamenridercraft/item/Fourze/FourzeDriverItem.java
@@ -58,6 +58,11 @@ import net.neoforged.neoforge.registries.DeferredItem;
 		@Override
 		public void Extra_set_Form_Item(ItemStack belt, Item ITEM,int SLOT,CompoundTag  tag)
 		{
+			if ((get_Form_Item(belt, 5) ==Fourze_Rider_Items.FOURZE_METEOR_NADESHIKO_FUSION_STATES.get() && ITEM !=Fourze_Rider_Items.FUSION_ASTROSWITCH.asItem())){
+
+		}
+
+
 			if ((get_Form_Item(belt, 5) ==Fourze_Rider_Items.FOURZE_ELEK_STATES.get() && get_Form_Item(belt, 1)!=Fourze_Rider_Items.ELEK_ASTROSWITCH.get())
 			  ||(get_Form_Item(belt, 5)==Fourze_Rider_Items.FOURZE_FIRE_STATES.get() && get_Form_Item(belt, 1)!=Fourze_Rider_Items.FIRE_ASTROSWITCH.get())
 					||(get_Form_Item(belt, 5)==Fourze_Rider_Items.FOURZE_ROCKET_DRILL_STATES.get() && get_Form_Item(belt, 1)!=Fourze_Rider_Items.CLEAR_DRILL_ASTROSWITCH.get())
@@ -65,11 +70,24 @@ import net.neoforged.neoforge.registries.DeferredItem;
 					||(get_Form_Item(belt, 5)==Fourze_Rider_Items.FOURZE_MAGNET_STATES.get() && get_Form_Item(belt, 4)!=Fourze_Rider_Items.MAGNET_ASTROSWITCH_S.get())
 			 ||(get_Form_Item(belt, 5)==Fourze_Rider_Items.FOURZE_ROCKET_STATES.get() && get_Form_Item(belt, 1)!=Fourze_Rider_Items.ROCKET_ASTROSWITCH.get())
 					||(get_Form_Item(belt, 5)==Fourze_Rider_Items.FOURZE_ROCKET_STATES.get() && get_Form_Item(belt, 4)!=Fourze_Rider_Items.SUPER_ROCKET_ASTROSWITCH.get()))
-				 {
+			 {
 				set_Form_Item(belt,Fourze_Rider_Items.FOURZE_BASE_STATES.asItem(), 5);
 			}
-if ((get_Form_Item(belt, 1)!=Fourze_Rider_Items.ROCKET_ASTROSWITCH.get() && get_Form_Item(belt, 4)==Fourze_Rider_Items.SUPER_ROCKET_ASTROSWITCH.get())){
-	set_Form_Item(belt,Fourze_Rider_Items.BLANK_SQUARE_ASTROSWITCH.asItem(), 4);
+
+			if (get_Form_Item(belt, 5) ==Fourze_Rider_Items.FOURZE_METEOR_NADESHIKO_FUSION_STATES.get()
+					||get_Form_Item(belt, 5) ==Fourze_Rider_Items.FOURZE_FUSION_STATES.get()){
+				if (get_Form_Item(belt, 1) !=Fourze_Rider_Items.BLANK_CIRCLE_ASTROSWITCH.get()
+						||get_Form_Item(belt, 2) !=Fourze_Rider_Items.BLANK_CROSS_ASTROSWITCH.get()
+						||get_Form_Item(belt, 3) !=Fourze_Rider_Items.BLANK_TRIANGLE_ASTROSWITCH.get()){
+					set_Form_Item(belt,Fourze_Rider_Items.FOURZE_BASE_STATES.asItem(), 5);
+					set_Form_Item(belt,Fourze_Rider_Items.BLANK_SQUARE_ASTROSWITCH.asItem(),4);
+
+				}
+
+			}
+
+			if ((get_Form_Item(belt, 1)!=Fourze_Rider_Items.ROCKET_ASTROSWITCH.get() && get_Form_Item(belt, 4)==Fourze_Rider_Items.SUPER_ROCKET_ASTROSWITCH.get())){
+			set_Form_Item(belt,Fourze_Rider_Items.BLANK_SQUARE_ASTROSWITCH.asItem(), 4);
 		}
 
 			if ((get_Form_Item(belt, 1) ==Fourze_Rider_Items.MAGNET_ASTROSWITCH_N.get()
@@ -82,6 +100,11 @@ if ((get_Form_Item(belt, 1)!=Fourze_Rider_Items.ROCKET_ASTROSWITCH.get() && get_
 					&&(get_Form_Item(belt, 3) !=Fourze_Rider_Items.BLANK_TRIANGLE_ASTROSWITCH.get() ))
 			{
 				set_Form_Item(belt,Fourze_Rider_Items.BLANK_CIRCLE_ASTROSWITCH.asItem(), 1);
+			}
+			if ((get_Form_Item(belt, 5) ==Fourze_Rider_Items.SHIN_CHAN_ASTROSWITCH.get())
+					&&(get_Form_Item(belt, 4) !=Fourze_Rider_Items.BLANK_SQUARE_ASTROSWITCH.get() ))
+			{
+				set_Form_Item(belt,Fourze_Rider_Items.FOURZE_BASE_STATES.asItem(), 5);
 			}
 
 
