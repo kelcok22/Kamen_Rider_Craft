3fe9f3d94db5efa7eaa083db974ac6a81abdee1a
kelcok22 <kelcok22@live.co.uk>
2024-12-28 22:52:10 +0000

    mach

--- src/main/java/com/kelco/kamenridercraft/item/drive/DriveDriverItem.java
+++ src/main/java/com/kelco/kamenridercraft/item/drive/DriveDriverItem.java
@@ -1,25 +1,13 @@
 package com.kelco.kamenridercraft.item.drive;
 
-import com.kelco.kamenridercraft.KamenRiderCraftCore;
-
-import com.kelco.kamenridercraft.item.BaseItems.RiderArmorItem;
 import com.kelco.kamenridercraft.item.BaseItems.RiderDriverItem;
-import com.kelco.kamenridercraft.item.BaseItems.RiderFormChangeItem;
-import com.kelco.kamenridercraft.item.Drive_Rider_Items;
-import com.kelco.kamenridercraft.item.Fourze_Rider_Items;
-import com.kelco.kamenridercraft.item.W_Rider_Items;
 import net.minecraft.core.Holder;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.registries.DeferredItem;
 
 import java.util.Objects;
