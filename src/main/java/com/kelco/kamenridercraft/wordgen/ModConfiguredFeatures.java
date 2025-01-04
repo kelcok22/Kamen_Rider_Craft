eed831f977c4c1a6e91caaf692a59a3c10b00b19
kelcok22 <12749084+kelcok22@users.noreply.github.com>
2025-01-04 22:25:20 +0000

    work on helheim

--- src/main/java/com/kelco/kamenridercraft/wordgen/ModConfiguredFeatures.java
+++ src/main/java/com/kelco/kamenridercraft/wordgen/ModConfiguredFeatures.java
@@ -11,6 +11,7 @@ import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
@@ -19,15 +20,17 @@ import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.DiskFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.VinesFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.*;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.SpruceFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.stateproviders.RotatedBlockProvider;
+import net.minecraft.world.level.levelgen.feature.stateproviders.RuleBasedBlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.treedecorators.LeaveVineDecorator;
 import net.minecraft.world.level.levelgen.feature.treedecorators.TrunkVineDecorator;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
@@ -37,12 +40,13 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.List;
+
 public class ModConfiguredFeatures {
 
     public static final DeferredRegister<Feature<?>> FEATURE = DeferredRegister.create(BuiltInRegistries.FEATURE,KamenRiderCraftCore.MOD_ID);
 
     public static final Holder<Feature<?>> HELHEIM_VINES = FEATURE.register("helheim_vines", () -> new helheimVineFeature(NoneFeatureConfiguration.CODEC));
-
 
     public static final ResourceKey<ConfiguredFeature<?, ?>> HELHEIM_TREE_KEY = registerKey("helheim_tree");
 
@@ -60,9 +64,7 @@ public class ModConfiguredFeatures {
                 BlockStateProvider.simple(Rider_Blocks.HELHEIM_LEAVES.get()),
                 new SpruceFoliagePlacer(ConstantInt.of(2), ConstantInt.of(2), UniformInt.of(3, 4)),
                 new TwoLayersFeatureSize(2, 0, 4))
-                                .decorators(ImmutableList.of(new TrunkVineDecorator(), TrunkVineDecorator.INSTANCE, new LeaveVineDecorator(0.25F)))
-                                .ignoreVines() .build());
-
+                .build());
 
     }
 
