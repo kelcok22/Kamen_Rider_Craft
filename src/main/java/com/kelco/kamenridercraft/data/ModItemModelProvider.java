15332273f541362e5b652fccfeaaeb21f525cbea
kelcok22 <kelcok22@live.co.uk>
2025-01-30 21:18:11 +0000

    added.model_has_different_name()

--- src/main/java/com/kelco/kamenridercraft/data/ModItemModelProvider.java
+++ src/main/java/com/kelco/kamenridercraft/data/ModItemModelProvider.java
@@ -2,17 +2,22 @@ package com.kelco.kamenridercraft.data;
 
 import com.kelco.kamenridercraft.KamenRiderCraftCore;
 import com.kelco.kamenridercraft.block.Rider_Blocks;
+import com.kelco.kamenridercraft.item.BaseItems.BaseItem;
 import com.kelco.kamenridercraft.item.tabs.RiderTabs;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.client.model.generators.ItemModelBuilder;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.registries.DeferredBlock;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ModItemModelProvider extends ItemModelProvider {
 
@@ -21,7 +26,7 @@ public class ModItemModelProvider extends ItemModelProvider {
         super(output, KamenRiderCraftCore.MOD_ID, existingFileHelper);
     }
 
-    public static List<Item> BASIC_ITEM_MODEL= new ArrayList<Item>();
+    public static List<BaseItem> BASIC_ITEM_MODEL= new ArrayList<BaseItem>();
 
     @Override
     protected void registerModels() {
@@ -29,7 +34,10 @@ public class ModItemModelProvider extends ItemModelProvider {
 
             for (int i = 0; i < BASIC_ITEM_MODEL.size(); i++)
             {
-                basicItem(BASIC_ITEM_MODEL.get(i));
+                ResourceLocation item = (ResourceLocation)Objects.requireNonNull(BuiltInRegistries.ITEM.getKey(BASIC_ITEM_MODEL.get(i)));
+                String item2 = BASIC_ITEM_MODEL.get(i).Model_Name==null? item.getPath():BASIC_ITEM_MODEL.get(i).Model_Name;
+                ((ItemModelBuilder) ((ItemModelBuilder) this.getBuilder(item.toString())).parent(new ModelFile.UncheckedModelFile("item/generated"))).texture("layer0", ResourceLocation.fromNamespaceAndPath(item.getNamespace(), "item/" + item2));
+
             }
 
 
@@ -40,6 +48,7 @@ public class ModItemModelProvider extends ItemModelProvider {
         basicItem(Rider_Blocks.HELHEIM_DOOR.asItem());
         basicItem(Rider_Blocks.RABBIT_HUTCH_DOOR.asItem());
     }
+
 
     public void buttonItem(DeferredBlock<?> block, DeferredBlock<Block> baseBlock) {
         this.withExistingParent(block.getId().getPath(), mcLoc("block/button_inventory"))
