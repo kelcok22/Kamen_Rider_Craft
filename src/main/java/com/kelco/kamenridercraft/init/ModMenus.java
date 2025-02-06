7633c0a36ebe5f6df510b7bb5fe047a2a1ccc65b
docrider23 <drwhofan23@live.com>
2025-02-06 01:30:50 -0500

    Advent Decks

--- src/main/java/com/kelco/kamenridercraft/init/ModMenus.java
+++ src/main/java/com/kelco/kamenridercraft/init/ModMenus.java
@@ -1,6 +1,7 @@
 package com.kelco.kamenridercraft.init;
 
 import com.kelco.kamenridercraft.KamenRiderCraftCore;
+import com.kelco.kamenridercraft.world.inventory.AdventDeckGuiMenu;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -14,6 +15,8 @@ import com.kelco.kamenridercraft.world.inventory.RingHolderGuiMenu;
 public class ModMenus {
 	public static final DeferredRegister<MenuType<?>> REGISTRY = DeferredRegister.create(Registries.MENU, KamenRiderCraftCore.MOD_ID);
 	public static final DeferredHolder<MenuType<?>, MenuType<RingHolderGuiMenu>> RING_HOLDER_GUI = REGISTRY.register("ring_holder_gui", () -> IMenuTypeExtension.create(RingHolderGuiMenu::new));
+	public static final DeferredHolder<MenuType<?>, MenuType<AdventDeckGuiMenu>> ADVENT_DECK_GUI = REGISTRY.register("advent_deck_gui", () -> IMenuTypeExtension.create(AdventDeckGuiMenu::new));
+
 
 	public static void register(IEventBus eventBus) {REGISTRY.register(eventBus);}
 }
