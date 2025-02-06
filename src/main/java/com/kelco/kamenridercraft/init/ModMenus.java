08f92e55dc5dcc791afe602d34d61b4fa290a022
docrider23 <drwhofan23@live.com>
2025-02-06 09:44:56 -0500

    Rouse Banks (yes, I consider the Blade Card Decks Rouse Banks, they are pretty much the same thing)

--- src/main/java/com/kelco/kamenridercraft/init/ModMenus.java
+++ src/main/java/com/kelco/kamenridercraft/init/ModMenus.java
@@ -1,7 +1,7 @@
 package com.kelco.kamenridercraft.init;
 
 import com.kelco.kamenridercraft.KamenRiderCraftCore;
-import com.kelco.kamenridercraft.world.inventory.AdventDeckGuiMenu;
+import com.kelco.kamenridercraft.world.inventory.*;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -10,12 +10,11 @@ import net.neoforged.neoforge.common.extensions.IMenuTypeExtension;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.core.registries.Registries;
 
-import com.kelco.kamenridercraft.world.inventory.RingHolderGuiMenu;
-
 public class ModMenus {
 	public static final DeferredRegister<MenuType<?>> REGISTRY = DeferredRegister.create(Registries.MENU, KamenRiderCraftCore.MOD_ID);
 	public static final DeferredHolder<MenuType<?>, MenuType<RingHolderGuiMenu>> RING_HOLDER_GUI = REGISTRY.register("ring_holder_gui", () -> IMenuTypeExtension.create(RingHolderGuiMenu::new));
 	public static final DeferredHolder<MenuType<?>, MenuType<AdventDeckGuiMenu>> ADVENT_DECK_GUI = REGISTRY.register("advent_deck_gui", () -> IMenuTypeExtension.create(AdventDeckGuiMenu::new));
+	public static final DeferredHolder<MenuType<?>, MenuType<RouseBankGuiMenu>> ROUSE_BANK_GUI = REGISTRY.register("rouse_bank_gui", () -> IMenuTypeExtension.create(RouseBankGuiMenu::new));
 
 
 	public static void register(IEventBus eventBus) {REGISTRY.register(eventBus);}
---
+++ src/main/java/com/kelco/kamenridercraft/item/blade/RouseBankItem.java
@@ -0,0 +1,58 @@
+
+package com.kelco.kamenridercraft.item.blade;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.FriendlyByteBuf;
+
+import io.netty.buffer.Unpooled;
+
+import com.kelco.kamenridercraft.world.inventory.RouseBankGuiMenu;
+
+import java.util.List;
+
+public class RouseBankItem extends Item {
+	public RouseBankItem() {
+		super(new Item.Properties().stacksTo(1).rarity(Rarity.COMMON));
+	}
+
+	public RouseBankItem AddToTabList(List<Item> TabList) {
+		TabList.add(this);
+		return this;
+	}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+		if (entity instanceof ServerPlayer serverPlayer) {
+			serverPlayer.openMenu(new MenuProvider() {
+				@Override
+				public Component getDisplayName() {
+					return Component.literal("Rouse Bank");
+				}
+
+				@Override
+				public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
+					FriendlyByteBuf packetBuffer = new FriendlyByteBuf(Unpooled.buffer());
+					packetBuffer.writeBlockPos(entity.blockPosition());
+					packetBuffer.writeByte(hand == InteractionHand.MAIN_HAND ? 0 : 1);
+					return new RouseBankGuiMenu(id, inventory, packetBuffer);
+				}
+			}, buf -> {
+				buf.writeBlockPos(entity.blockPosition());
+				buf.writeByte(hand == InteractionHand.MAIN_HAND ? 0 : 1);
+			});
+		}
+		return ar;
+	}
+}
