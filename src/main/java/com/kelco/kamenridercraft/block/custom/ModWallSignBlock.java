7981242a715b4df52f3e5036fd50a29e7764c50a
Lovekov <168786855+LovekovGaming@users.noreply.github.com>
2025-02-12 14:56:36 -0800

    Kuzu

--- src/main/java/com/kelco/kamenridercraft/block/custom/ModWallSignBlock.java
+++ src/main/java/com/kelco/kamenridercraft/block/custom/ModWallSignBlock.java
@@ -1,6 +1,9 @@
 package com.kelco.kamenridercraft.block.custom;
 
+import com.kelco.kamenridercraft.block.Rider_Blocks;
 import com.kelco.kamenridercraft.block.entity.ModSignBlockEntity;
+
+import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.WallSignBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -10,6 +13,11 @@ import net.minecraft.world.level.block.state.properties.WoodType;
 public class ModWallSignBlock extends WallSignBlock {
     public ModWallSignBlock(WoodType type, Properties properties) {
         super(type, properties);
+    }
+
+    @Override
+    public String getDescriptionId() {
+        return Util.makeDescriptionId("item", Rider_Blocks.HELHEIM_WALL_SIGN.getId());
     }
 
     @Override
