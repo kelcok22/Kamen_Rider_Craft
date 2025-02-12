69bfda9c673f1c78eb7e91a1261fe436726eb217
kelcok22 <kelcok22@live.co.uk>
2025-02-12 04:04:10 +0000

    still not saving

--- src/main/java/com/kelco/kamenridercraft/item/ryuki/AdventDeckItem.java
+++ src/main/java/com/kelco/kamenridercraft/item/ryuki/AdventDeckItem.java
@@ -2,7 +2,11 @@
 package com.kelco.kamenridercraft.item.ryuki;
 
 /*import com.kelco.kamenridercraft.procedures.OpenAdventDeckProcedure;*/
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.Container;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.ItemContainerContents;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
@@ -21,11 +25,14 @@ import io.netty.buffer.Unpooled;
 
 import com.kelco.kamenridercraft.world.inventory.AdventDeckGuiMenu;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class AdventDeckItem extends Item {
+	private static final Component UNKNOWN_CONTENTS = Component.translatable("container.shulkerBox.unknownContents");
+
 	public AdventDeckItem() {
-		super(new Item.Properties().stacksTo(1).rarity(Rarity.COMMON));
+		super(new Item.Properties().stacksTo(1).rarity(Rarity.COMMON).component(DataComponents.CONTAINER, ItemContainerContents.EMPTY));
 	}
 
 	public AdventDeckItem AddToTabList(List<Item> TabList) {
@@ -59,7 +66,7 @@ public class AdventDeckItem extends Item {
 					FriendlyByteBuf packetBuffer = new FriendlyByteBuf(Unpooled.buffer());
 					packetBuffer.writeBlockPos(entity.blockPosition());
 					packetBuffer.writeByte(hand == InteractionHand.MAIN_HAND ? 0 : 1);
-					return new AdventDeckGuiMenu(id, inventory, packetBuffer);
+					return new AdventDeckGuiMenu(id, inventory, packetBuffer,itemstack);
 				}
 			}, buf -> {
 				buf.writeBlockPos(entity.blockPosition());
@@ -70,4 +77,28 @@ public class AdventDeckItem extends Item {
 		return InteractionResultHolder.sidedSuccess(itemstack, world.isClientSide());
 	}
 
+	public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+		super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+		if (stack.has(DataComponents.CONTAINER_LOOT)) {
+			tooltipComponents.add(UNKNOWN_CONTENTS);
+		}
+
+		int i = 0;
+		int j = 0;
+		Iterator var7 = ((ItemContainerContents)stack.getOrDefault(DataComponents.CONTAINER, ItemContainerContents.EMPTY)).nonEmptyItems().iterator();
+
+		while(var7.hasNext()) {
+			ItemStack itemstack = (ItemStack)var7.next();
+			++j;
+			if (i <= 4) {
+				++i;
+				tooltipComponents.add(Component.translatable("container.shulkerBox.itemCount", new Object[]{itemstack.getHoverName(), itemstack.getCount()}));
+			}
+		}
+
+		if (j - i > 0) {
+			tooltipComponents.add(Component.translatable("container.shulkerBox.more", new Object[]{j - i}).withStyle(ChatFormatting.ITALIC));
+		}
+
+	}
 }
