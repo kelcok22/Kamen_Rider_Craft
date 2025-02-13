60cbb915c23ac620574bec5c33a57e2663679e09
Hs-CandC <70107353+Hs-CandC@users.noreply.github.com>
2025-02-13 16:36:16 +0100

    signs continued

--- src/main/java/com/kelco/kamenridercraft/block/entity/ModHangingSignBlockEntity.java
+++ src/main/java/com/kelco/kamenridercraft/block/entity/ModHangingSignBlockEntity.java
@@ -3,12 +3,13 @@ package com.kelco.kamenridercraft.block.entity;
 import com.kelco.kamenridercraft.entities.footSoldiers.BaseHenchmenEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.HangingSignBlockEntity;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class ModHangingSignBlockEntity extends SignBlockEntity {
+public class ModHangingSignBlockEntity extends HangingSignBlockEntity {
     public ModHangingSignBlockEntity(BlockPos pos, BlockState blockState) {
-        super(ModBlockEntities.MOD_HANGING_SIGN.get(), pos, blockState);
+        super(pos, blockState);
     }
 
     @Override
