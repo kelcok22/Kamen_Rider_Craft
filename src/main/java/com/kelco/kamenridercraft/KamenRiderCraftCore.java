422493ab0fdf97c519fbb6fb1723f5aa1ab33d66
Hs-CandC <70107353+Hs-CandC@users.noreply.github.com>
2025-02-18 01:16:09 +0100

    astroswitch rack start

--- src/main/java/com/kelco/kamenridercraft/KamenRiderCraftCore.java
+++ src/main/java/com/kelco/kamenridercraft/KamenRiderCraftCore.java
@@ -480,6 +480,7 @@ public class KamenRiderCraftCore
             /*event.register(ModMenus.ROUSE_BANK_GUI.get(), RouseBankGuiScreen::new);
             event.register(ModMenus.FUESLOT_GUI.get(), FueslotGuiScreen::new);*/
             event.register(ModMenus.ASTROSWITCH_PANEL_GUI.get(), AstroswitchPanelGuiScreen::new);
+            event.register(ModMenus.ASTROSWITCH_PANEL_DOUBLE_GUI.get(), AstroswitchPanelDoubleGuiScreen::new);
             event.register(ModMenus.RIDE_BOOKER_GUI.get(), RideBookerGuiScreen::new);
             event.register(ModMenus.PANDORA_PANEL_GUI.get(), PandoraPanelGuiScreen::new);
 
