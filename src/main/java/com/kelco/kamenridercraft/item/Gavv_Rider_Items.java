098ac1a3eb052da4362327493761c4ccbb737430
Hs-CandC <70107353+Hs-CandC@users.noreply.github.com>
2025-02-24 00:48:50 +0100

    Valen Tirol

--- src/main/java/com/kelco/kamenridercraft/item/Gavv_Rider_Items.java
+++ src/main/java/com/kelco/kamenridercraft/item/Gavv_Rider_Items.java
@@ -253,7 +253,9 @@ public class Gavv_Rider_Items {
 					.AddToList(CHOCO,1).AddToList(RiderTabs.GAVV_TAB_ITEM).has_basic_model());
 
 	public static final DeferredItem<Item> TIROLCHOCOLATE_GOCHIZO_VARIETY = ITEMS.register("tirolchocolate_gochizo_variety",
-			() -> new RiderFormChangeItem(new Item.Properties(),0,"_tirolchocolate","valen","valenbuckle_belt")
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_tirolchocolate","valen","valenbuckle_belt",
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 0,true,false)
+					,new MobEffectInstance(Effect_core.SHOT_BOOST, 40, 0,true,false))
 					.AddToList(CHOCO,2).AddToList(RiderTabs.GAVV_TAB_ITEM).has_basic_model());
 
 	public static final DeferredItem<Item> STRAWBERRYJELLYTIROL_GOCHIZO = ITEMS.register("strawberryjellytirol_gochizo",
