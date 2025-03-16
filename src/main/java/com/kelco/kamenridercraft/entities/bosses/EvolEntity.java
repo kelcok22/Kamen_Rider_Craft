02623678a9cad6046b9f15650b579675f66cb768
Hs-CandC <70107353+Hs-CandC@users.noreply.github.com>
2025-03-16 11:24:19 +0100

    Evol Rework

--- src/main/java/com/kelco/kamenridercraft/entities/bosses/EvolEntity.java
+++ src/main/java/com/kelco/kamenridercraft/entities/bosses/EvolEntity.java
@@ -1,8 +1,10 @@
 package com.kelco.kamenridercraft.entities.bosses;
 
+import com.kelco.kamenridercraft.block.Rider_Blocks;
 import com.kelco.kamenridercraft.entities.footSoldiers.BaseHenchmenEntity;
 import com.kelco.kamenridercraft.item.BaseItems.RiderDriverItem;
 import com.kelco.kamenridercraft.item.Build_Rider_Items;
+import com.kelco.kamenridercraft.item.Saber_Rider_Items;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
@@ -11,6 +13,7 @@ import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
@@ -30,15 +33,24 @@ public class EvolEntity extends BaseHenchmenEntity {
 	@Override
     public void actuallyHurt(DamageSource source, float amount) {
         super.actuallyHurt(source, amount);
-    	if(!this.level().isClientSide() && source.getEntity() instanceof Player playerIn && this.getHealth()<100
-		&& this.getItemBySlot(EquipmentSlot.FEET).getItem()==Build_Rider_Items.EVOL_DRIVER.get() && RiderDriverItem.get_Form_Item(this.getItemBySlot(EquipmentSlot.FEET),1)!=Build_Rider_Items.EVOL_TRIGGER_KAIJIN.get()) {
-			playerIn.sendSystemMessage(Component.translatable("henshin.kamenridercraft.evolto"));
+    	if(!this.level().isClientSide() && source.getEntity() instanceof Player playerIn && this.getHealth()<100)
+        if (playerIn.getInventory().countItem(Item.byBlock(Rider_Blocks.PANDORA_BOX.get()))!=0 && RiderDriverItem.get_Form_Item(this.getItemBySlot(EquipmentSlot.FEET),1)!=Build_Rider_Items.EVOL_TRIGGER.get()) {
+            playerIn.sendSystemMessage(Component.translatable("henshin.kamenridercraft.evol_black_hole"));
 			this.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.5);
-			this.getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(10.0D);
+			this.getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(12.0D);
 			this.getAttribute(Attributes.FOLLOW_RANGE).setBaseValue(128.0D);
-			RiderDriverItem.set_Form_Item(this.getItemBySlot(EquipmentSlot.FEET), Build_Rider_Items.EVOL_TRIGGER_KAIJIN.get(), 1);
+			RiderDriverItem.set_Form_Item(this.getItemBySlot(EquipmentSlot.FEET), Build_Rider_Items.EVOL_TRIGGER.get(), 1);
     	}
-    }    
+        if(!this.level().isClientSide() && source.getEntity() instanceof Player playerIn && this.getHealth()<50)
+            if (playerIn.getInventory().countItem(Build_Rider_Items.LAST_PANDORA_PANEL_BLACK.get())!=0 && RiderDriverItem.get_Form_Item(this.getItemBySlot(EquipmentSlot.FEET),1)!=Build_Rider_Items.EVOL_TRIGGER.get()) {
+                playerIn.sendSystemMessage(Component.translatable("henshin.kamenridercraft.evolto"));
+                this.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(1);
+                this.getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(15.0D);
+                this.getAttribute(Attributes.FOLLOW_RANGE).setBaseValue(128.0D);
+                RiderDriverItem.set_Form_Item(this.getItemBySlot(EquipmentSlot.FEET), Build_Rider_Items.EVOL_TRIGGER_KAIJIN.get(), 1);
+            }
+    }
+
 
     public static AttributeSupplier.Builder setAttributes() {
 
--- src/main/java/com/kelco/kamenridercraft/item/Build_Rider_Items.java
+++ src/main/java/com/kelco/kamenridercraft/item/Build_Rider_Items.java
@@ -49,6 +49,12 @@ public class Build_Rider_Items {
 	public static final DeferredItem<Item> GOLDEN_BANGLE= ITEMS.register("golden_bangle",
 			() -> new BaseItem(new Item.Properties()).AddToList(RiderTabs.BUILD_TAB_ITEM));
 
+	public static final DeferredItem<Item> PANDORA_PANEL_TOUTO = ITEMS.register("pandora_panel_touto", () -> new PandoraPanelItem(Rider_Blocks.PANDORA_PANEL_BLOCK.get()).AddToList(RiderTabs.BUILD_TAB_ITEM));
+	public static final DeferredItem<Item> PANDORA_PANEL_HOKUTO = ITEMS.register("pandora_panel_hokuto", () -> new PandoraPanelItem(Rider_Blocks.PANDORA_PANEL_BLOCK.get()).AddToList(RiderTabs.BUILD_TAB_ITEM));
+	public static final DeferredItem<Item> PANDORA_PANEL_SEITO = ITEMS.register("pandora_panel_seito", () -> new PandoraPanelItem(Rider_Blocks.PANDORA_PANEL_BLOCK.get()).AddToList(RiderTabs.BUILD_TAB_ITEM));
+	public static final DeferredItem<Item> LAST_PANDORA_PANEL_BLACK = ITEMS.register("last_pandora_panel_black", () -> new PandoraPanelItem(Rider_Blocks.PANDORA_PANEL_BLOCK.get()).AddToList(RiderTabs.BUILD_TAB_ITEM));
+	public static final DeferredItem<Item> LAST_PANDORA_PANEL_WHITE = ITEMS.register("last_pandora_panel_white", () -> new PandoraPanelItem(Rider_Blocks.PANDORA_PANEL_BLOCK.get()).AddToList(RiderTabs.BUILD_TAB_ITEM));
+
 
 	public static final DeferredItem<Item> RABBIT_FULL_BOTTLE = ITEMS.register("rabbit_full_bottle",
 			() -> new FullBottleItem(new Item.Properties(),0,"_rabbit","build","build_driver_belt",
@@ -699,7 +705,7 @@ public class Build_Rider_Items {
 					new MobEffectInstance(MobEffects.DIG_SPEED, 40, 2,true,false),
 					new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0,true,false),
 					new MobEffectInstance(Effect_core.FLYING, 40, 0,true,false))
-					.model_has_different_name("evol_trigger").has_basic_model());
+					.addNeedItem(LAST_PANDORA_PANEL_BLACK.get()).model_has_different_name("evol_trigger").has_basic_model());
 
 	public static final DeferredItem<Item> EVOL_TRIGGER = ITEMS.register("evol_trigger",
 			() -> new RiderFormChangeItem(new Item.Properties(),0,"_black_hole","evol","evol_driver_belt_b",
@@ -1035,11 +1041,6 @@ public class Build_Rider_Items {
 	public static final DeferredItem<Item> NANBA_WALKING_STICK = ITEMS.register("nanba_walking_stick",
 			() -> new BaseSwordItem(Tiers.DIAMOND, 1, -2.4F, new Item.Properties()).AddToTabList(RiderTabs.BUILD_TAB_ITEM).ChangeRepairItem(FULL_BOTTLE.get()));
 
-	public static final DeferredItem<Item> PANDORA_PANEL_TOUTO = ITEMS.register("pandora_panel_touto", () -> new PandoraPanelItem(Rider_Blocks.PANDORA_PANEL_BLOCK.get()).AddToList(RiderTabs.BUILD_TAB_ITEM));
-	public static final DeferredItem<Item> PANDORA_PANEL_HOKUTO = ITEMS.register("pandora_panel_hokuto", () -> new PandoraPanelItem(Rider_Blocks.PANDORA_PANEL_BLOCK.get()).AddToList(RiderTabs.BUILD_TAB_ITEM));
-	public static final DeferredItem<Item> PANDORA_PANEL_SEITO = ITEMS.register("pandora_panel_seito", () -> new PandoraPanelItem(Rider_Blocks.PANDORA_PANEL_BLOCK.get()).AddToList(RiderTabs.BUILD_TAB_ITEM));
-	public static final DeferredItem<Item> LAST_PANDORA_PANEL_BLACK = ITEMS.register("last_pandora_panel_black", () -> new PandoraPanelItem(Rider_Blocks.PANDORA_PANEL_BLOCK.get()).AddToList(RiderTabs.BUILD_TAB_ITEM));
-	public static final DeferredItem<Item> LAST_PANDORA_PANEL_WHITE = ITEMS.register("last_pandora_panel_white", () -> new PandoraPanelItem(Rider_Blocks.PANDORA_PANEL_BLOCK.get()).AddToList(RiderTabs.BUILD_TAB_ITEM));
 
 	public static final DeferredItem<Item> FULLBOTTLE_HOLDER = ITEMS.register("fullbottle_holder",
 			() -> new FullBottleHolderItem());
