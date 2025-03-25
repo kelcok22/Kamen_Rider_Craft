fb13102e71d652fad29212f2f739cbdba99cd725
kelcok22 <12749084+kelcok22@users.noreply.github.com>
2025-03-25 00:56:56 +0000

    fixes

---
+++ src/main/java/com/kelco/kamenridercraft/item/BaseItems/component/slot/CaseArmorSlot.java
@@ -0,0 +1,66 @@
+package com.kelco.kamenridercraft.item.BaseItems.component.slot;
+
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentEffectComponents;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+
+import javax.annotation.Nullable;
+
+public class CaseArmorSlot extends Slot {
+    private final LivingEntity owner;
+    private final EquipmentSlot slot;
+    @Nullable
+    private final ResourceLocation emptyIcon;
+
+    public CaseArmorSlot(
+            Container container, LivingEntity owner, EquipmentSlot slot, int slotIndex, int x, int y, @Nullable ResourceLocation emptyIcon
+    ) {
+        super(container, slotIndex, x, y);
+        this.owner = owner;
+        this.slot = slot;
+        this.emptyIcon = emptyIcon;
+    }
+
+    @Override
+    public void setByPlayer(ItemStack newStack, ItemStack oldStack) {
+        this.owner.onEquipItem(this.slot, oldStack, newStack);
+        super.setByPlayer(newStack, oldStack);
+    }
+
+    @Override
+    public int getMaxStackSize() {
+        return 1;
+    }
+
+    /**
+     * Check if the stack is allowed to be placed in this slot, used for armor slots as well as furnace fuel.
+     */
+    @Override
+    public boolean mayPlace(ItemStack stack) {
+        return stack.canEquip(slot, owner);
+    }
+
+    /**
+     * Return whether this slot's stack can be taken from this slot.
+     */
+    @Override
+    public boolean mayPickup(Player player) {
+        ItemStack itemstack = this.getItem();
+        return !itemstack.isEmpty() && !player.isCreative() && EnchantmentHelper.has(itemstack, EnchantmentEffectComponents.PREVENT_ARMOR_CHANGE)
+                ? false
+                : super.mayPickup(player);
+    }
+
+    @Override
+    public Pair<ResourceLocation, ResourceLocation> getNoItemIcon() {
+        return this.emptyIcon != null ? Pair.of(InventoryMenu.BLOCK_ATLAS, this.emptyIcon) : super.getNoItemIcon();
+    }
+}
--- src/main/java/com/kelco/kamenridercraft/world/inventory/RiderCaseGuiMenu.java
+++ src/main/java/com/kelco/kamenridercraft/world/inventory/RiderCaseGuiMenu.java
@@ -3,24 +3,44 @@ package com.kelco.kamenridercraft.world.inventory;
 import com.kelco.kamenridercraft.KamenRiderCraftCore;
 import com.kelco.kamenridercraft.init.ModMenus;
 import com.kelco.kamenridercraft.item.BaseItems.component.BasicContainer;
+import com.kelco.kamenridercraft.item.BaseItems.component.slot.CaseArmorSlot;
 import com.kelco.kamenridercraft.item.BaseItems.component.slot.SlotByTag;
 import com.kelco.kamenridercraft.item.Fourze_Rider_Items;
 import com.kelco.kamenridercraft.item.Modded_item_core;
+import com.kelco.kamenridercraft.item.W_Rider_Items;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.Container;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
-import net.neoforged.neoforge.items.SlotItemHandler;
+
+import javax.annotation.Nullable;
+import java.util.Map;
 
 public class RiderCaseGuiMenu extends AbstractContainerMenu {
     private static final int CONTAINER_SIZE = 10;
     private final Container container;
+    public static final ResourceLocation EMPTY_ARMOR_SLOT_HELMET = ResourceLocation.withDefaultNamespace("item/empty_armor_slot_helmet");
+    public static final ResourceLocation EMPTY_ARMOR_SLOT_CHESTPLATE = ResourceLocation.withDefaultNamespace("item/empty_armor_slot_chestplate");
+    public static final ResourceLocation EMPTY_ARMOR_SLOT_LEGGINGS = ResourceLocation.withDefaultNamespace("item/empty_armor_slot_leggings");
+    public static final ResourceLocation EMPTY_ARMOR_SLOT_BOOTS = ResourceLocation.withDefaultNamespace("item/empty_armor_slot_boots");
+    public static final ResourceLocation EMPTY_ARMOR_SLOT_SHIELD = ResourceLocation.withDefaultNamespace("item/empty_armor_slot_shield");
 
+    private static final Map<EquipmentSlot, ResourceLocation> TEXTURE_EMPTY_SLOTS = Map.of(
+            EquipmentSlot.FEET,
+            EMPTY_ARMOR_SLOT_BOOTS,
+            EquipmentSlot.LEGS,
+            EMPTY_ARMOR_SLOT_LEGGINGS,
+            EquipmentSlot.CHEST,
+            EMPTY_ARMOR_SLOT_CHESTPLATE,
+            EquipmentSlot.HEAD,
+            EMPTY_ARMOR_SLOT_HELMET
+    );
     public RiderCaseGuiMenu(int containerId, Inventory playerInventory, FriendlyByteBuf registryFriendlyByteBuf) {
         this(containerId, playerInventory,new ItemStack(Modded_item_core.GENERIC_RIDER_CASE.get()));
     }
@@ -74,19 +94,32 @@ public class RiderCaseGuiMenu extends AbstractContainerMenu {
         this.addSlot(new SlotByTag(container,8,126,52,"gear/all_items"));
         this.addSlot(new SlotByTag(container,9,144,52,"gear/all_items"));
 
-        this.addSlot(new Slot(playerInventory, 39,26,16));
-        this.addSlot(new Slot(playerInventory, 38,26,34));
-        this.addSlot(new Slot(playerInventory, 37,26,52));
-        this.addSlot(new Slot(playerInventory, 36,44,34));
+
+        this.addSlot(new CaseArmorSlot(playerInventory, playerInventory.player, EquipmentSlot.HEAD, 39,26,16, TEXTURE_EMPTY_SLOTS.get(EquipmentSlot.HEAD)));
+        this.addSlot(new CaseArmorSlot(playerInventory, playerInventory.player, EquipmentSlot.CHEST, 38,26,34, TEXTURE_EMPTY_SLOTS.get(EquipmentSlot.CHEST)));
+        this.addSlot(new CaseArmorSlot(playerInventory, playerInventory.player, EquipmentSlot.LEGS, 37,26,52, TEXTURE_EMPTY_SLOTS.get(EquipmentSlot.LEGS)));
+        this.addSlot(new CaseArmorSlot(playerInventory, playerInventory.player, EquipmentSlot.FEET, 36,44,34, TEXTURE_EMPTY_SLOTS.get(EquipmentSlot.FEET)));
 
         for(int i1 = 0; i1 < 3; ++i1) {
             for(int k1 = 0; k1 < 9; ++k1) {
-                this.addSlot(new Slot(playerInventory, k1 + i1 * 9 + 9, 8 + k1 * 18, 84 + i1 * 18));
+                this.addSlot(new Slot(playerInventory, k1 + i1 * 9 + 9, 8 + k1 * 18, 84 + i1 * 18){
+                    @Override
+                    public boolean mayPickup(Player player) {
+                        if (this.getItem().getItem()==Modded_item_core.GENERIC_RIDER_CASE.get())return false;
+                        return true;
+                    }
+                });
             }
         }
 
         for(int j1 = 0; j1 < 9; ++j1) {
-            this.addSlot(new Slot(playerInventory, j1, 8 + j1 * 18, 142));
+            this.addSlot(new Slot(playerInventory, j1, 8 + j1 * 18, 142){
+                @Override
+                public boolean mayPickup(Player player) {
+                    if (this.getItem().getItem()==Modded_item_core.GENERIC_RIDER_CASE.get())return false;
+                    return true;
+                }
+            });
         }
     }
 
