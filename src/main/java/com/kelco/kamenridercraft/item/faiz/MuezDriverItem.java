5dca9bb1a5cbd72670d6ad2078df7932131caa72
Lovekov <168786855+LovekovGaming@users.noreply.github.com>
2025-04-03 20:22:48 -0700

    Patchouli rewrite: Zi-O onward + Ichigo, other series

--- src/main/java/com/kelco/kamenridercraft/item/faiz/MuezDriverItem.java
+++ src/main/java/com/kelco/kamenridercraft/item/faiz/MuezDriverItem.java
@@ -10,7 +10,9 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Item;
@@ -30,8 +32,13 @@ public class MuezDriverItem extends RiderDriverItem {
 
     @Override
     public String getUnlimitedTextures(ItemStack itemstack, EquipmentSlot equipmentSlot, LivingEntity rider, String riderName ,int num) {
-        if (num==1&&rider.getMainHandItem().getItem()!=Faiz_Rider_Items.MUEZ_EDGE.get()) return "muez_edge_l";
-        else if (num==2&&rider.getOffhandItem().getItem()!=Faiz_Rider_Items.MUEZ_EDGE.get()) return "muez_edge_r";
+        if ((rider instanceof Player || rider instanceof Mob) && rider.getMainArm() == HumanoidArm.LEFT) {
+            if (num==1&&rider.getOffhandItem().getItem()!=Faiz_Rider_Items.MUEZ_EDGE.get()) return "muez_edge_l";
+            else if (num==2&&rider.getMainHandItem().getItem()!=Faiz_Rider_Items.MUEZ_EDGE.get()) return "muez_edge_r";
+        } else {
+            if (num==1&&rider.getMainHandItem().getItem()!=Faiz_Rider_Items.MUEZ_EDGE.get()) return "muez_edge_l";
+            else if (num==2&&rider.getOffhandItem().getItem()!=Faiz_Rider_Items.MUEZ_EDGE.get()) return "muez_edge_r";
+        }
         return "blank";
     }
 
