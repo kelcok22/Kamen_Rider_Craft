c3ec7dfed69c36c7f004c6df68a89b6a083a4eef
kelcok22 <kelcok22@live.co.uk>
2025-04-17 00:56:33 +0100

    skull crystal

--- src/main/java/com/kelco/kamenridercraft/item/Decade_Rider_Items.java
+++ src/main/java/com/kelco/kamenridercraft/item/Decade_Rider_Items.java
@@ -748,7 +748,7 @@ public class Decade_Rider_Items {
 	public static final DeferredItem<Item> KUUGA_BALL = ITEMS.register("kuuga_ball",
 			() -> new RiderFormChangeItem(new Item.Properties(),0,"_ball","kuuga","blank",
 					new MobEffectInstance(Effect_core.PUNCH, 40, 2,true,false))
-					.IsGlowing().ChangeModel("kuuga_ball.geo.json","kuuga_ball.animation.json").SetPalyerModelInvisible().AddToList(RiderTabs.DECADE_TAB_ITEM));
+					.IsGlowing().ChangeModel("kuuga_ball.geo.json","kuuga_ball.animation.json").SetPalyerModelInvisible().has_basic_model().AddToList(RiderTabs.DECADE_TAB_ITEM));
 
 	public static final DeferredItem<Item> DECADEHELMET = ITEMS.register("decadehead",
 			() -> new RiderArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.HELMET, new Item.Properties()).AddToTabList(RiderTabs.DECADE_TAB_ITEM).ChangeRepairItem(BLANK_CARD.get()));
