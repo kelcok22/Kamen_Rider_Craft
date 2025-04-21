5cbfa72832865daa5a65f4ca82903c1617193357
kelcok22 <12749084+kelcok22@users.noreply.github.com>
2025-04-21 02:00:41 +0100

    Kuuga black eyes forms

--- src/main/java/com/kelco/kamenridercraft/item/Kuuga_Rider_Items.java
+++ src/main/java/com/kelco/kamenridercraft/item/Kuuga_Rider_Items.java
@@ -110,6 +110,15 @@ public class Kuuga_Rider_Items {
             		new MobEffectInstance(Effect_core.PUNCH, 40, 5,true,false))
 					.IsBeltGlowing().IsGlowing().AddToList(RiderTabs.KUUGA_TAB_ITEM));
 
+	public static final DeferredItem<Item> KUUGA_ULTIMATE_BLACK_EYES = ITEMS.register("kuuga_ultimate_black_eyes",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_ultimate_black_eyes","kuuga","arcle_belt_u",
+					new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 3,true,false),
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
+					new MobEffectInstance(MobEffects.JUMP, 40, 3,true,false),
+					new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 4,true,false),
+					new MobEffectInstance(Effect_core.PUNCH, 40, 5,true,false))
+					.IsBeltGlowing().IsGlowing().has_basic_model().AddToList(RiderTabs.KUUGA_TAB_ITEM));
+
     public static final DeferredItem<Item> KUUGA_RISING_ULTIMATE = ITEMS.register("kuuga_rising_ultimate",
             () -> new RiderFormChangeItem(new Item.Properties(),0,"_rising_ultimate","kuuga","arcle_belt_ru",
             		new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 3,true,false),
@@ -120,7 +129,17 @@ public class Kuuga_Rider_Items {
             		new MobEffectInstance(Effect_core.PUNCH, 40, 5,true,false))
 					.IsGlowing().AddToList(RiderTabs.KUUGA_TAB_ITEM));
 
-    public static final DeferredItem<Item> KUUGA_SUPER_RISING_ULTIMATE = ITEMS.register("kuuga_super_rising_ultimate",
+	public static final DeferredItem<Item> KUUGA_RISING_ULTIMATE_BLACK_EYES = ITEMS.register("kuuga_rising_ultimate_black_eyes",
+			() -> new RiderFormChangeItem(new Item.Properties(),0,"_rising_ultimate_black_eyes","kuuga","arcle_belt_ru",
+					new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 3,true,false),
+					new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 4,true,false),
+					new MobEffectInstance(MobEffects.JUMP, 40, 4,true,false),
+					new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 5,true,false),
+					new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0,true,false),
+					new MobEffectInstance(Effect_core.PUNCH, 40, 5,true,false))
+					.IsGlowing().has_basic_model().AddToList(RiderTabs.KUUGA_TAB_ITEM));
+
+	public static final DeferredItem<Item> KUUGA_SUPER_RISING_ULTIMATE = ITEMS.register("kuuga_super_rising_ultimate",
     		() -> new RiderFormChangeItem(new Item.Properties(),0,"_super_rising_ultimate","kuuga","arcle_belt_u",
             		new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 3,true,false),
             		new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 5,true,false),
