b48b9ccde5cd447336a6b3081b7cb7405f0fb783
Lovekov <168786855+LovekovGaming@users.noreply.github.com>
2025-06-09 14:19:57 -0700

    Update ArkZeroEntity.java ONORE KELCOOOOOOO

--- src/main/java/com/kelco/kamenridercraft/entities/bosses/ArkZeroEntity.java
+++ src/main/java/com/kelco/kamenridercraft/entities/bosses/ArkZeroEntity.java
@@ -45,6 +45,20 @@ public class ArkZeroEntity extends BaseHenchmenEntity {
         this.setItemSlot(EquipmentSlot.FEET, new ItemStack(Zero_One_Rider_Items.ARK_DRIVER_ZERO.get()));
     }
 
+	@Override
+    public void actuallyHurt(DamageSource source, float amount) {
+        super.actuallyHurt(source, amount);
+    	if(!this.level().isClientSide() && source.getEntity() instanceof Player playerIn && this.getHealth()<100
+		&& this.getItemBySlot(EquipmentSlot.FEET).getItem()==Zero_One_Rider_Items.ARK_DRIVER_ZERO.get() && RiderDriverItem.get_Form_Item(this.getItemBySlot(EquipmentSlot.FEET),1)!=Zero_One_Rider_Items.ARK_ONE_PROGRISEKEY.get()) {
+			playerIn.sendSystemMessage(Component.translatable("henshin.kamenridercraft.ark_one"));
+    				
+    		this.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.5);
+    		this.getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(10.0D);
+    		this.getAttribute(Attributes.FOLLOW_RANGE).setBaseValue(128.0D);
+    	    RiderDriverItem.set_Form_Item(this.getItemBySlot(EquipmentSlot.FEET), Zero_One_Rider_Items.ARK_ONE_PROGRISEKEY.get(), 1);
+        }
+    }
+
     protected void customServerAiStep() {
         super.customServerAiStep();
                 this.bossEvent.setName(Component.translatable("entity.kamenridercraft.ark_zero").withStyle(ChatFormatting.RED));;
