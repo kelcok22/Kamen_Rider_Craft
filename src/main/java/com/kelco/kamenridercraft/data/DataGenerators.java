06f910b5c09155ad344743f0d0691f84f24f690d
Hs-CandC <70107353+Hs-CandC@users.noreply.github.com>
2025-06-25 19:31:21 +0200

    various fixes

--- src/main/java/com/kelco/kamenridercraft/data/DataGenerators.java
+++ src/main/java/com/kelco/kamenridercraft/data/DataGenerators.java
@@ -28,7 +28,7 @@ public class DataGenerators {
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
-       // generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(),
+      //  generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(),
       //          List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
 
        BlockTagsProvider blockTagsProvider = new ModBlockTagProvider(packOutput, lookupProvider, existingFileHelper);
--- src/main/java/com/kelco/kamenridercraft/data/ModBlockLootTableProvider.java
+++ src/main/java/com/kelco/kamenridercraft/data/ModBlockLootTableProvider.java
@@ -1,14 +1,28 @@
 package com.kelco.kamenridercraft.data;
 
+import com.google.common.collect.Sets;
+import com.kelco.kamenridercraft.KamenRiderCraftCore;
 import com.kelco.kamenridercraft.block.Rider_Blocks;
 import com.kelco.kamenridercraft.item.Modded_item_core;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.ValidationContext;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ModBlockLootTableProvider extends BlockLootSubProvider {
     protected ModBlockLootTableProvider(HolderLookup.Provider registries) {
@@ -17,83 +31,89 @@ public class ModBlockLootTableProvider extends BlockLootSubProvider {
 
     @Override
     protected void generate() {
-        dropSelf(Rider_Blocks.HELHEIM_BUTTON.get());
-        add(Rider_Blocks.HELHEIM_SLAB.get(),
-                block -> createSlabItemTable(Rider_Blocks.HELHEIM_SLAB.get()));
-        add(Rider_Blocks.WALLPLATE_SLAB.get(),
-                block -> createSlabItemTable(Rider_Blocks.WALLPLATE_SLAB.get()));
-        add(Rider_Blocks.WHITE_WALLPLATE_SLAB.get(),
-                block -> createSlabItemTable(Rider_Blocks.WHITE_WALLPLATE_SLAB.get()));
-        add(Rider_Blocks.RED_WALLPLATE_SLAB.get(),
-                block -> createSlabItemTable(Rider_Blocks.RED_WALLPLATE_SLAB.get()));
-        add(Rider_Blocks.GREY_WALLPLATE_SLAB.get(),
-                block -> createSlabItemTable(Rider_Blocks.GREY_WALLPLATE_SLAB.get()));
-        add(Rider_Blocks.GREEN_WALLPLATE_SLAB.get(),
-                block -> createSlabItemTable(Rider_Blocks.GREEN_WALLPLATE_SLAB.get()));
-        add(Rider_Blocks.LIGHT_GREEN_WALLPLATE_SLAB.get(),
-                block -> createSlabItemTable(Rider_Blocks.LIGHT_GREEN_WALLPLATE_SLAB.get()));
-        add(Rider_Blocks.BLACK_WALLPLATE_SLAB.get(),
-                block -> createSlabItemTable(Rider_Blocks.BLACK_WALLPLATE_SLAB.get()));
-        add(Rider_Blocks.YELLOW_WALLPLATE_SLAB.get(),
-                block -> createSlabItemTable(Rider_Blocks.YELLOW_WALLPLATE_SLAB.get()));
-        add(Rider_Blocks.LIGHT_GREEN_WALLPLATE_GRATE_SLAB.get(),
-                block -> createSlabItemTable(Rider_Blocks.LIGHT_GREEN_WALLPLATE_GRATE_SLAB.get()));
+        //dropSelf(Rider_Blocks.HELHEIM_BUTTON.get());
+        //add(Rider_Blocks.HELHEIM_SLAB.get(),
+        //        block -> createSlabItemTable(Rider_Blocks.HELHEIM_SLAB.get()));
+        //add(Rider_Blocks.WALLPLATE_SLAB.get(),
+        //        block -> createSlabItemTable(Rider_Blocks.WALLPLATE_SLAB.get()));
+        //add(Rider_Blocks.WHITE_WALLPLATE_SLAB.get(),
+        //        block -> createSlabItemTable(Rider_Blocks.WHITE_WALLPLATE_SLAB.get()));
+        //add(Rider_Blocks.RED_WALLPLATE_SLAB.get(),
+        //        block -> createSlabItemTable(Rider_Blocks.RED_WALLPLATE_SLAB.get()));
+        //add(Rider_Blocks.GREY_WALLPLATE_SLAB.get(),
+        //        block -> createSlabItemTable(Rider_Blocks.GREY_WALLPLATE_SLAB.get()));
+        //add(Rider_Blocks.GREEN_WALLPLATE_SLAB.get(),
+        //        block -> createSlabItemTable(Rider_Blocks.GREEN_WALLPLATE_SLAB.get()));
+        //add(Rider_Blocks.LIGHT_GREEN_WALLPLATE_SLAB.get(),
+        //        block -> createSlabItemTable(Rider_Blocks.LIGHT_GREEN_WALLPLATE_SLAB.get()));
+        //add(Rider_Blocks.BLACK_WALLPLATE_SLAB.get(),
+        //        block -> createSlabItemTable(Rider_Blocks.BLACK_WALLPLATE_SLAB.get()));
+        //add(Rider_Blocks.LIGHT_GREEN_WALLPLATE_GRATE_SLAB.get(),
+        //        block -> createSlabItemTable(Rider_Blocks.LIGHT_GREEN_WALLPLATE_GRATE_SLAB.get()));
 
-        dropSelf(Rider_Blocks.CANDY_SHOP.get());
-        dropSelf(Rider_Blocks.HELHEIM_STAIRS.get());
-        dropSelf(Rider_Blocks.HELHEIM_PRESSURE_PLATE.get());
-        dropSelf(Rider_Blocks.HELHEIM_FENCE.get());
-        dropSelf(Rider_Blocks.WHITE_FENCE.get());
-        dropSelf(Rider_Blocks.HELHEIM_FENCE_GATE.get());
-        dropSelf(Rider_Blocks.HELHEIM_TRAPDOOR.get());
-        dropSelf(Rider_Blocks.CORNERSTORE_SIGN.get());
-        dropSelf(Rider_Blocks.YELLOW_WALLPLATE.get());
-        dropSelf(Rider_Blocks.GREY_WALLPLATE.get());
-        dropSelf(Rider_Blocks.BLACK_WALLPLATE.get());
-        dropSelf(Rider_Blocks.GREEN_WALLPLATE.get());
-        dropSelf(Rider_Blocks.LIGHT_GREEN_WALLPLATE.get());
-        dropSelf(Rider_Blocks.RED_WALLPLATE.get());
-        dropSelf(Rider_Blocks.YELLOW_WALLPLATE_STAIRS.get());
-        dropSelf(Rider_Blocks.GREY_WALLPLATE_STAIRS.get());
-        dropSelf(Rider_Blocks.BLACK_WALLPLATE_STAIRS.get());
-        dropSelf(Rider_Blocks.GREEN_WALLPLATE_STAIRS.get());
-        dropSelf(Rider_Blocks.LIGHT_GREEN_WALLPLATE_STAIRS.get());
-        dropSelf(Rider_Blocks.RED_WALLPLATE_STAIRS.get());
-        dropSelf(Rider_Blocks.WALLPLATE_STAIRS.get());
-        dropSelf(Rider_Blocks.CHECKERED_TILE.get());
-        dropSelf(Rider_Blocks.YELLOW_CHECKERED_TILE.get());
-        dropSelf(Rider_Blocks.LIME_CHECKERED_TILE.get());
-        dropSelf(Rider_Blocks.VERTICAL_PANEL.get());
-        dropSelf(Rider_Blocks.PLINTH.get());
-        dropSelf(Rider_Blocks.PAVEMENT_ROADLINE.get());
-        add(Rider_Blocks.HELHEIM_DOOR.get(),
-                block -> createDoorTable(Rider_Blocks.HELHEIM_DOOR.get()));
-        dropSelf(Rider_Blocks.ICHIGO_CHAIR.get());
-        dropSelf(Rider_Blocks.HELHEIM_SAPLING.get());
-        add(Rider_Blocks.RABBIT_HUTCH_DOOR.get(),
-                block -> createDoorTable(Rider_Blocks.RABBIT_HUTCH_DOOR.get()));
-        add(Rider_Blocks.GLASS_DOOR.get(),
-                block -> createDoorTable(Rider_Blocks.GLASS_DOOR.get()));
-        add(Rider_Blocks.WONDERWOOD_DOOR.get(),
-                block -> createDoorTable(Rider_Blocks.WONDERWOOD_DOOR.get()));
-        add(Rider_Blocks.JAIL_DOOR.get(),
-                block -> createDoorTable(Rider_Blocks.JAIL_DOOR.get()));
+        //dropSelf(Rider_Blocks.CANDY_SHOP.get());
+        //dropSelf(Rider_Blocks.HELHEIM_STAIRS.get());
+        //dropSelf(Rider_Blocks.HELHEIM_PRESSURE_PLATE.get());
+        //dropSelf(Rider_Blocks.WHITE_FENCE.get());
+        //dropSelf(Rider_Blocks.HELHEIM_FENCE_GATE.get());
+        //dropSelf(Rider_Blocks.HELHEIM_TRAPDOOR.get());
+        //dropSelf(Rider_Blocks.CORNERSTORE_SIGN.get());
 
-        add(Rider_Blocks.HELHEIM_SIGN.get(),
-                block -> createSingleItemTable(Modded_item_core.HELHEIM_SIGN_ITEM.get()));
-        add(Rider_Blocks.HELHEIM_WALL_SIGN.get(),
-                block -> createSingleItemTable(Modded_item_core.HELHEIM_SIGN_ITEM.get()));
-        add(Rider_Blocks.HELHEIM_HANGING_SIGN.get(),
-                block -> createSingleItemTable(Modded_item_core.HELHEIM_HANGING_SIGN_ITEM.get()));
-        add(Rider_Blocks.HELHEIM_WALL_HANGING_SIGN.get(),
-                block -> createSingleItemTable(Modded_item_core.HELHEIM_HANGING_SIGN_ITEM.get()));
+        //dropSelf(Rider_Blocks.YELLOW_WALLPLATE.get());
+        //dropSelf(Rider_Blocks.GREY_WALLPLATE.get());
+        //dropSelf(Rider_Blocks.BLACK_WALLPLATE.get());
+        //dropSelf(Rider_Blocks.GREEN_WALLPLATE.get());
+        //dropSelf(Rider_Blocks.LIGHT_GREEN_WALLPLATE.get());
+        //dropSelf(Rider_Blocks.RED_WALLPLATE.get());
+        //dropSelf(Rider_Blocks.YELLOW_WALLPLATE_STAIRS.get());
+        //dropSelf(Rider_Blocks.GREY_WALLPLATE_STAIRS.get());
+        //dropSelf(Rider_Blocks.BLACK_WALLPLATE_STAIRS.get());
+        //dropSelf(Rider_Blocks.GREEN_WALLPLATE_STAIRS.get());
+        //dropSelf(Rider_Blocks.LIGHT_GREEN_WALLPLATE_STAIRS.get());
+        //dropSelf(Rider_Blocks.RED_WALLPLATE_STAIRS.get());
+        //dropSelf(Rider_Blocks.WALLPLATE_STAIRS.get());
+
+        //dropSelf(Rider_Blocks.CHECKERED_TILE.get());
+        //dropSelf(Rider_Blocks.YELLOW_CHECKERED_TILE.get());
+        //dropSelf(Rider_Blocks.LIME_CHECKERED_TILE.get());
+        //dropSelf(Rider_Blocks.VERTICAL_PANEL.get());
+        //dropSelf(Rider_Blocks.PLINTH.get());
+        //dropSelf(Rider_Blocks.PAVEMENT_ROADLINE.get());
+        dropSelf(Rider_Blocks.CAFE_COUNTER.get());
+
+        //add(Rider_Blocks.HELHEIM_DOOR.get(),
+        //        block -> createDoorTable(Rider_Blocks.HELHEIM_DOOR.get()));
+        //dropSelf(Rider_Blocks.ICHIGO_CHAIR.get());
+        //dropSelf(Rider_Blocks.HELHEIM_SAPLING.get());
+        //add(Rider_Blocks.RABBIT_HUTCH_DOOR.get(),
+        //        block -> createDoorTable(Rider_Blocks.RABBIT_HUTCH_DOOR.get()));
+        //add(Rider_Blocks.GLASS_DOOR.get(),
+        //        block -> createDoorTable(Rider_Blocks.GLASS_DOOR.get()));
+        //add(Rider_Blocks.WONDERWOOD_DOOR.get(),
+        //        block -> createDoorTable(Rider_Blocks.WONDERWOOD_DOOR.get()));
+        //add(Rider_Blocks.JAIL_DOOR.get(),
+        //        block -> createDoorTable(Rider_Blocks.JAIL_DOOR.get()));
+
+       // add(Rider_Blocks.HELHEIM_SIGN.get(),
+       //         block -> createSingleItemTable(Modded_item_core.HELHEIM_SIGN_ITEM.get()));
+        //add(Rider_Blocks.HELHEIM_WALL_SIGN.get(),
+        //        block -> createSingleItemTable(Modded_item_core.HELHEIM_SIGN_ITEM.get()));
+       // add(Rider_Blocks.HELHEIM_HANGING_SIGN.get(),
+       //         block -> createSingleItemTable(Modded_item_core.HELHEIM_HANGING_SIGN_ITEM.get()));
+       // add(Rider_Blocks.HELHEIM_WALL_HANGING_SIGN.get(),
+       //         block -> createSingleItemTable(Modded_item_core.HELHEIM_HANGING_SIGN_ITEM.get()));
         add(Rider_Blocks.YELLOW_WALLPLATE_WALL.get(),
                 block -> createSingleItemTable(Rider_Blocks.YELLOW_WALLPLATE_WALL.get()));
 
     }
 
     @Override
-    protected Iterable<Block> getKnownBlocks() {
-        return Rider_Blocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+    public @NotNull Iterable<Block> getKnownBlocks() {
+        return BuiltInRegistries.BLOCK.stream()
+                .filter(block -> Optional.of(BuiltInRegistries.BLOCK.getKey(block))
+                        .filter(key -> key.getNamespace().equals(KamenRiderCraftCore.MOD_ID))
+                        .isPresent())
+                .collect(Collectors.toSet());
     }
 }
+
