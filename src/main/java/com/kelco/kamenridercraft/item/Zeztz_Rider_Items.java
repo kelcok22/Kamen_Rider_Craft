90fcf1b2448dc63a9a2a7382e27b0ed0106b6d41
kelcok22 <12749084+kelcok22@users.noreply.github.com>
2025-07-07 15:53:40 +0100

    Zeztz done for now?

--- src/main/java/com/kelco/kamenridercraft/item/Zeztz_Rider_Items.java
+++ src/main/java/com/kelco/kamenridercraft/item/Zeztz_Rider_Items.java
@@ -23,14 +23,14 @@ public class Zeztz_Rider_Items {
                 new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 0,true,false),
                 new MobEffectInstance(MobEffects.DIG_SPEED, 40, 1,true,false),
                 new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 0,true,false))
-               .ChangeBeltModel("geo/zeztz_riderbelt.geo.json").IsBeltGlowing().IsGlowing().AddToList(RiderTabs.ZEZTZ_TAB_ITEM));
+               .ChangeBeltModel("geo/zeztz_riderbelt.geo.json").IsBeltGlowing().IsGlowing().has_basic_model().AddToList(RiderTabs.ZEZTZ_TAB_ITEM));
 
         public static final DeferredItem<Item> ZEZTZ_HELMET = ITEMS.register("zeztz_head",
                 () -> new RiderArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.HELMET, new Item.Properties()).has_basic_model().AddToTabList(RiderTabs.ZEZTZ_TAB_ITEM));
         public static final DeferredItem<Item> ZEZTZ_CHESTPLATE = ITEMS.register("zeztz_troso",
-                () -> new RiderArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.CHESTPLATE, new Item.Properties()).AddToTabList(RiderTabs.ZEZTZ_TAB_ITEM));
+                () -> new RiderArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.CHESTPLATE, new Item.Properties()).has_basic_model().AddToTabList(RiderTabs.ZEZTZ_TAB_ITEM));
         public static final DeferredItem<Item> ZEZTZ_LEGGINGS = ITEMS.register("zeztz_legs",
-                () -> new RiderArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.LEGGINGS, new Item.Properties()).AddToTabList(RiderTabs.ZEZTZ_TAB_ITEM));
+                () -> new RiderArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.LEGGINGS, new Item.Properties()).has_basic_model().AddToTabList(RiderTabs.ZEZTZ_TAB_ITEM));
 
             
         public static final DeferredItem<Item> ZEZTZ_DRIVER = ITEMS.register("zeztz_driver",
