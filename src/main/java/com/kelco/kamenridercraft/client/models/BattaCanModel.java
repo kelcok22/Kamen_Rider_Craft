4ad0392d82d4491c50ef1754f1b177277c4453ee
LiasDan <44766439+LiasDan@users.noreply.github.com>
2025-07-25 19:51:21 +0800

    Batta Can

---
+++ src/main/java/com/kelco/kamenridercraft/client/models/BattaCanModel.java
@@ -0,0 +1,26 @@
+package com.kelco.kamenridercraft.client.models;
+
+
+import com.kelco.kamenridercraft.KamenRiderCraftCore;
+import com.kelco.kamenridercraft.entities.allies.BattaCanEntity;
+import com.kelco.kamenridercraft.entities.allies.TakaCanEntity;
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.model.GeoModel;
+
+public class BattaCanModel extends GeoModel<BattaCanEntity> {
+    @Override
+    public ResourceLocation getModelResource(BattaCanEntity animatable) {
+        return  ResourceLocation.fromNamespaceAndPath(KamenRiderCraftCore.MOD_ID, "geo/batta_can.geo.json");
+    }
+
+    @Override
+    public ResourceLocation getTextureResource(BattaCanEntity animatable) {
+        return ResourceLocation.fromNamespaceAndPath(KamenRiderCraftCore.MOD_ID, "textures/entity/batta_can.png");
+    }
+
+    @Override
+    public ResourceLocation getAnimationResource(BattaCanEntity animatable) {
+        return ResourceLocation.fromNamespaceAndPath(KamenRiderCraftCore.MOD_ID, "animations/batta_can.animation.json");
+    }
+
+}
