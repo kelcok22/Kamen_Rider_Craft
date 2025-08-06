13c024fff55ef33d4a12a09c32904139ea412b2d
BuildTools <168786855+IMakeaTheMod@users.noreply.github.com>
2025-08-05 19:42:03 -0700

    I only did what I needed for the sound pack

--- src/main/java/com/kelco/kamenridercraft/block/entity/ModSignBlockEntity.java
+++ src/main/java/com/kelco/kamenridercraft/block/entity/ModSignBlockEntity.java
@@ -1,6 +1,5 @@
 package com.kelco.kamenridercraft.block.entity;
 
-import com.kelco.kamenridercraft.entities.footSoldiers.BaseHenchmenEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
