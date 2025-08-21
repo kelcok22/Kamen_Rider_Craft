d6542cea965a4a1d03f253f7e70fe2cd1da6b537
LiasDan <44766439+LiasDan@users.noreply.github.com>
2025-08-21 21:00:29 +0800

    GotcharBruh again...

--- src/main/java/com/kelco/kamenridercraft/item/gotchard/GotcharDriverItem.java
+++ src/main/java/com/kelco/kamenridercraft/item/gotchard/GotcharDriverItem.java
@@ -33,7 +33,7 @@ public class GotcharDriverItem extends RiderDriverItem {
 	}
 
 	public ResourceLocation getBeltModelResource(ItemStack itemstack,RiderArmorItem animatable, EquipmentSlot slot, LivingEntity rider) {
-		if(get_Form_Item(itemstack,1).get_Belt_Model()=="geo/gotchard_belt_big.geo.json" && !isTransformed(rider)) {
+		if (get_Form_Item(itemstack,1).get_Belt_Model()=="geo/gotchard_belt_big.geo.json" && !isTransformed(rider)) {
 			return ResourceLocation.fromNamespaceAndPath(KamenRiderCraftCore.MOD_ID,"geo/gotchard_belt.geo.json");
 		}
 
