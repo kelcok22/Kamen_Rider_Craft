d3b31b8a79da6f60620ad7fc50e6344d832fd7f4
kelco <kelcok22@live.co.uk>
2025-10-10 22:54:16 +0100

    last fixes?

--- src/main/java/com/kelco/kamenridercraft/item/Ichigo_Rider_Items.java
+++ src/main/java/com/kelco/kamenridercraft/item/Ichigo_Rider_Items.java
@@ -205,10 +205,10 @@ public class Ichigo_Rider_Items {
                     ,new MobEffectInstance(MobEffects.JUMP, 40, 2,true,false))
                     .SetShowFace().AddToList(RiderTabs.ICHIGO_TAB_ITEM));
 
-    public static final DeferredItem<Item> SHOCKER_SPIRIT = ITEMS.register("shocker_spirit",
-            () -> new RiderFormChangeItem(new Item.Properties(),0,"","shocker_combatman","shocker_belt",
-                    new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false))
-                    .SetShowFace().AddToList(RiderTabs.ICHIGO_TAB_ITEM));
+    //public static final DeferredItem<Item> SHOCKER_SPIRIT = ITEMS.register("shocker_spirit",
+    //         () -> new RiderFormChangeItem(new Item.Properties(),0,"","shocker_combatman","shocker_belt",
+    //                new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false))
+    //                .SetShowFace().AddToList(RiderTabs.ICHIGO_TAB_ITEM));
 
 
     public static final DeferredItem<Item> ICHIGOHELMET = ITEMS.register("ichigohead",
@@ -298,8 +298,8 @@ public class Ichigo_Rider_Items {
     public static final DeferredItem<Item> GAIA_BELT = ITEMS.register("gaia_belt",
             () -> new RiderDriverItem(ArmorMaterials.DIAMOND,"gaia",GAIA_CORE,ICHIGOHELMET, ICHIGOCHESTPLATE,ICHIGOLEGGINGS , new Item.Properties().rarity(Rarity.UNCOMMON)).has_basic_model().AddToTabList(RiderTabs.ICHIGO_TAB_ITEM));
 
-    public static final DeferredItem<Item> SHOCKER_BELT = ITEMS.register("shocker_belt",
-            () -> new RiderDriverItem(ArmorMaterials.DIAMOND,"shocker_combatman",SHOCKER_SPIRIT ,ICHIGOHELMET, ICHIGOCHESTPLATE,ICHIGOLEGGINGS , new Item.Properties()).Dont_show_belt_form_info().IsA1().AddToTabList(RiderTabs.ICHIGO_TAB_ITEM));
+    //public static final DeferredItem<Item> SHOCKER_BELT = ITEMS.register("shocker_belt",
+    //        () -> new RiderDriverItem(ArmorMaterials.DIAMOND,"shocker_combatman",SHOCKER_SPIRIT ,ICHIGOHELMET, ICHIGOCHESTPLATE,ICHIGOLEGGINGS , new Item.Properties()).Dont_show_belt_form_info().IsA1().AddToTabList(RiderTabs.ICHIGO_TAB_ITEM));
 
     //V3
 
