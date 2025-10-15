d2cf2bcfa75570aa54612beb79daf0bf5a40e232
kelco <kelcok22@live.co.uk>
2025-10-15 02:54:03 +0100

    start of ghost rework

---
+++ src/main/java/com/kelco/kamenridercraft/block/machineBlocks/Monolith.java
@@ -0,0 +1,72 @@
+package com.kelco.kamenridercraft.block.machineBlocks;
+
+
+import com.kelco.kamenridercraft.item.Build_Rider_Items;
+import com.kelco.kamenridercraft.item.Ghost_Rider_Items;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class Monolith extends MachineBlock {
+
+	  public static List<Item> GHOST_EYECONS= new ArrayList<>();
+
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
+    public Monolith(Properties properties) {
+        super(properties);
+    }
+
+
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_53681_) {
+        p_53681_.add(FACING);
+     }
+
+     public BlockState getStateForPlacement(BlockPlaceContext p_53679_) {
+        return this.defaultBlockState().setValue(FACING, p_53679_.getHorizontalDirection().getOpposite());
+     }
+
+    private Item getEyeconDrop() {
+ 		Random generator = new Random();
+ 			int rand = generator.nextInt(GHOST_EYECONS.size());
+ 			return GHOST_EYECONS.get(rand);
+ 	}
+
+    @Override
+    protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+
+        if (!level.isClientSide()) {
+            if (player.getItemInHand(hand).getItem() == Ghost_Rider_Items.BLANK_GHOST_EYECON.get()){
+                process(player, level, pos, hand, getEyeconDrop());
+                return ItemInteractionResult.SUCCESS;
+            }
+
+        }
+        return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+    }
+
+
+
+	public Monolith AddToTabList(List<Block> TabList) {
+		TabList.add(this);
+		return this;
+	}
+
+}
