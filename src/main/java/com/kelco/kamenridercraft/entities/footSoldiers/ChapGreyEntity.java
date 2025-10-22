55e97497acd1f74cf6af2cd27e8f0410d9ab0009
kelco <kelcok22@live.co.uk>
2025-10-22 17:56:46 +0100

    remove ghost ore gen

--- src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/ChapGreyEntity.java
+++ src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/ChapGreyEntity.java
@@ -1,13 +1,23 @@
 package com.kelco.kamenridercraft.entities.footSoldiers;
 
 
+import com.kelco.kamenridercraft.block.Rider_Blocks;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+
+import java.time.LocalDate;
 
 public class ChapGreyEntity extends BaseHenchmenEntity {
 	
     public ChapGreyEntity(EntityType<? extends BaseHenchmenEntity> type, Level level) {
         super(type, level);
         NAME="chaps_grey";
+        LocalDate localdate = LocalDate.now();
+        int day = localdate.getDayOfMonth();
+        if (localdate.getMonthValue() == 10 && day >= 24) {
+            this.setItemSlot(EquipmentSlot.HEAD, new ItemStack(Rider_Blocks.FERBUS.get()));
+        }
     }
 }
