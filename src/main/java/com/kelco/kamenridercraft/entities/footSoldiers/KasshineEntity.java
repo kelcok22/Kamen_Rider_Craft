3f729749f5e4dad6d5d9d48a9b5782d59a743483
AndrewBean684 <andrewbean100@gmail.com>
2025-10-29 23:22:19 -0400

    another zi-o mob part final i hope

--- src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/KasshineEntity.java
+++ src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/KasshineEntity.java
@@ -1,11 +1,15 @@
 package com.kelco.kamenridercraft.entities.footSoldiers;
 
+import com.kelco.kamenridercraft.entities.MobsCore;
 import com.kelco.kamenridercraft.item.Zi_O_Rider_Items;
+import com.kelco.kamenridercraft.level.ModGameRules;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
@@ -21,7 +25,24 @@ public class KasshineEntity extends BaseHenchmenEntity {
         this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Zi_O_Rider_Items.KASSHINE_TRIDENT.get()));
     }
 
-    
+    public void remove(RemovalReason p_149847_) {
+        if ( this.isDeadOrDying()) {
+            if (this.random.nextDouble() * 100.0 <= this.level().getGameRules().getInt(ModGameRules.RULE_BOSS_SPAWN_PERCENTAGE)) {
+                BaseHenchmenEntity boss = MobsCore.ANOTHER_ZI_O.get().create(this.level());
+                if (boss != null) {
+                    boss.moveTo(this.getX(), this.getY(), this.getZ(), this.getYRot(), 0.0F);
+                    this.level().addFreshEntity(boss);
+                    if (this.getLastAttacker()instanceof Player playerIn && this.level().getGameRules().getBoolean(ModGameRules.RULE_BOSS_HENSHIN_ANNOUCEMENTS)) {
+                        playerIn.sendSystemMessage(Component.translatable("henshin.kamenridercraft.another_zi_o"));
+                    }
+                }
+            }
+        }
+        super.remove(p_149847_);
+    }
+
+
+
     public static AttributeSupplier.Builder setAttributes() {
     
         return Monster.createMonsterAttributes()
