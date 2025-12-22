9bedd1b9bf85e575b5b6758549c55919485150b1
kelco <>
2025-12-22 00:09:51 +0000

    Chambara Gamer Lv. 3(Zombie)

--- src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/ShockerCombatmanEntity.java
+++ src/main/java/com/kelco/kamenridercraft/entities/footSoldiers/ShockerCombatmanEntity.java
@@ -4,10 +4,13 @@ import com.kelco.kamenridercraft.item.Ghost_Rider_Items;
 import com.kelco.kamenridercraft.item.Miscellaneous_Rider_Items;
 import com.kelco.kamenridercraft.item.Modded_item_core;
 import com.kelco.kamenridercraft.level.ModGameRules;
+import com.kelco.kamenridercraft.particle.ModParticles;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 
 import com.kelco.kamenridercraft.entities.MobsCore;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
@@ -23,7 +26,7 @@ public class ShockerCombatmanEntity extends BaseHenchmenEntity {
 		super(type, level);
         LocalDate localdate = LocalDate.now();
         int day = localdate.getDayOfMonth();
-        if (localdate.getMonthValue() == 12 && day >= 22 && day <= 28 ) NAME="shocker_combatman_christmas";
+        if (localdate.getMonthValue() == 12 && day >= 21 && day <= 28 ) NAME="shocker_combatman_christmas";
         else NAME="shocker_combatman";
 
 	}
@@ -32,12 +35,14 @@ public class ShockerCombatmanEntity extends BaseHenchmenEntity {
 		if (reason == RemovalReason.KILLED) {
             LocalDate localdate = LocalDate.now();
             int day = localdate.getDayOfMonth();
-            if (localdate.getMonthValue() == 12 && day >= 22 && day <= 28   && this.lastHurtByPlayer!=null){
+            if (localdate.getMonthValue() == 12 && day >= 21 && day <= 28   && this.lastHurtByPlayer!=null){
                 ItemEntity key = new ItemEntity(level(), getX(), getY(), getZ(), new ItemStack(Miscellaneous_Rider_Items.GIFT.get(), 1), 0, 0, 0);
                 key.setPickUpDelay(0);
                 level().addFreshEntity(key);
             }
-
+            ((ServerLevel) this.level()).sendParticles(ParticleTypes.CLOUD,
+                    this.getX() + 0, this.getY(),
+                    this.getZ() + 0, 100, 0, 0, 0, 0.1);
 
             if (this.getLastAttacker()instanceof Player playerIn){}
                 if (this.random.nextDouble() * 100.0 <= this.level().getGameRules().getInt(ModGameRules.RULE_BOSS_SPAWN_PERCENTAGE)) {
