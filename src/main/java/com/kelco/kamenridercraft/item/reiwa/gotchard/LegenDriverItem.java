b29d1c10b7fe4dba3b252e21d7a768181068db50
Lovekov <168786855+LovekovGaming@users.noreply.github.com>
2026-05-09 20:05:22 -0700

    仮面ライダー響鬼 スーパーモードを更新する

--- src/main/java/com/kelco/kamenridercraft/item/reiwa/gotchard/LegenDriverItem.java
+++ src/main/java/com/kelco/kamenridercraft/item/reiwa/gotchard/LegenDriverItem.java
@@ -3,11 +3,15 @@ package com.kelco.kamenridercraft.item.reiwa.gotchard;
 import com.kelco.kamenridercraft.KamenRiderCraftCore;
 import com.kelco.kamenridercraft.item.base_items.RiderArmorItem;
 import com.kelco.kamenridercraft.item.base_items.RiderDriverItem;
+import com.kelco.kamenridercraft.item.heisei_phase_1.Hibiki_Rider_Items;
 import com.kelco.kamenridercraft.item.reiwa.Gotchard_Rider_Items;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -19,6 +23,25 @@ public class LegenDriverItem extends RiderDriverItem {
 
 	public LegenDriverItem(Holder<ArmorMaterial> material, String rider, DeferredItem<Item> baseFormItem, DeferredItem<Item> head, DeferredItem<Item> torso, DeferredItem<Item> legs, Properties properties) {
 		super(material, rider, baseFormItem, head, torso, legs, properties);
+		Unlimited_Textures = 2;
+	}
+
+	@Override
+	public String getUnlimitedTextures(ItemStack itemstack, LivingEntity rider, String riderName ,int num) {
+		if (get_Form_Item(itemstack, 1) == Gotchard_Rider_Items.ARMED_HIBIKI_RIDE_CHEMY_CARD.get()) {
+			if ((rider instanceof Player || rider instanceof Mob) && rider.getMainArm() == HumanoidArm.LEFT) {
+				if (num == 1 && rider.getMainHandItem().getItem() != Hibiki_Rider_Items.ONGEKIBO_REKKA.get())
+					return "ongekibo_rekka_l";
+				else if (num == 2 && rider.getOffhandItem().getItem() != Hibiki_Rider_Items.ONGEKIBO_REKKA.get())
+					return "ongekibo_rekka_r";
+			} else {
+				if (num == 1 && rider.getOffhandItem().getItem() != Hibiki_Rider_Items.ONGEKIBO_REKKA.get())
+					return "ongekibo_rekka_l";
+				else if (num == 2 && rider.getMainHandItem().getItem() != Hibiki_Rider_Items.ONGEKIBO_REKKA.get())
+					return "ongekibo_rekka_r";
+			}
+		}
+		return "blank";
 	}
 
 	@Override
