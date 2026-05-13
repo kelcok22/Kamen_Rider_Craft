5b8efac7b290a2fcfc69d9a8c3a8429db96920f5
Blurtle <>
2026-05-13 09:11:23 -0600

    Tojima test 3!

--- src/main/java/com/kelco/kamenridercraft/item/base_items/MaskItem.java
+++ src/main/java/com/kelco/kamenridercraft/item/base_items/MaskItem.java
@@ -11,9 +11,6 @@ import net.minecraft.world.item.Equipable;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
-import static net.minecraft.core.Direction.Axis.X;
-import static net.minecraft.core.Direction.Axis.Z;
-
 public class MaskItem extends BaseItem implements Equipable {
 
     public MaskItem(Properties prop) {
@@ -23,9 +20,8 @@ public class MaskItem extends BaseItem implements Equipable {
     @Override
     public void inventoryTick(ItemStack stack, Level level, Entity entity, int slotId, boolean isSelected) {
         super.inventoryTick(stack, level, entity, slotId, isSelected);
-        if (slotId == 39 && this.toString().equals("kamenridercraft:ichigo_mask") && level instanceof ServerLevel serverLevel && ((LivingEntity) entity).hasEffect(EffectCore.RIDER_SPIRIT)) {
-            serverLevel.sendParticles(ParticleTypes.RAIN, entity.getX() + (entity.getLookAngle().get(X) * 0.2), entity.getEyeY() + 0.15, entity.getZ() + (entity.getLookAngle().get(Z) * 0.2), 2,0, 0,0, 0.05);
-
+        if (slotId == 39 && this.toString().equals("kamenridercraft:ichigo_mask") && ((LivingEntity) entity).hasEffect(EffectCore.RIDER_SPIRIT)) {
+            level.addParticle(ParticleTypes.RAIN, entity.getX(), entity.getEyeY(), entity.getZ(), 0, 1, 0);
         }
     }
 
