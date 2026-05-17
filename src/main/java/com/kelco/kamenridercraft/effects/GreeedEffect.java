344eaee3141cb0951b0f53984cc707db88b0e9b3
kelco <>
2026-05-17 23:32:13 +0100

    more bug fixes

--- src/main/java/com/kelco/kamenridercraft/effects/GreeedEffect.java
+++ src/main/java/com/kelco/kamenridercraft/effects/GreeedEffect.java
@@ -2,9 +2,11 @@ package com.kelco.kamenridercraft.effects;
 
 import com.kelco.kamenridercraft.entity.mobs.MobsCore;
 import com.kelco.kamenridercraft.entity.mobs.foot_soldiers.BaseHenchmenEntity;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.levelgen.Heightmap;
 
 import java.util.Random;
 
@@ -23,11 +25,19 @@ public class GreeedEffect extends MobEffect {
 
 	@Override
 	public boolean applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
+
 		BaseHenchmenEntity boss  =   MobsCore.YUMMY.get().create(pLivingEntity.level());
 		Random rand = new Random();
         if ((pAmplifier < 50 ? rand.nextInt(500 - (pAmplifier * 10)) : 0) == 0) {
             if (boss != null) {
-                boss.moveTo(pLivingEntity.getX() + (rand.nextInt(8) - 4), pLivingEntity.getY(), pLivingEntity.getZ() + (rand.nextInt(8) - 4), 0.0f, 0.0F);
+
+                int X= (int) (pLivingEntity.getX() + (rand.nextInt(8) - 4));
+                int Y= (int) pLivingEntity.getY();
+                int Z= (int) (pLivingEntity.getZ() + (rand.nextInt(8) - 4));
+                BlockPos pos = new BlockPos(X,Y,Z);
+                int i = pLivingEntity.level().getChunkAt(pos).getHeight(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, pos .getX(), pos.getZ()) + 1;
+                if(i>0)boss.moveTo(X,i,Z);
+                else boss.moveTo(X,Y,Z);
                 pLivingEntity.level().addFreshEntity(boss);
             }
         }
