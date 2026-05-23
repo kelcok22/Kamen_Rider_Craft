3b2170ec02133c9697b2e49da7796d0f839ca744
kelco <>
2026-05-23 01:18:58 +0100

    my balls killed the update

--- src/main/java/com/kelco/kamenridercraft/events/ModClientEvents.java
+++ src/main/java/com/kelco/kamenridercraft/events/ModClientEvents.java
@@ -1,10 +1,20 @@
 package com.kelco.kamenridercraft.events;
 
+import com.kelco.kamenridercraft.item.base_items.RiderDriverItem;
+import com.kelco.kamenridercraft.network.payload.AttributeChangePayload;
 import com.kelco.kamenridercraft.network.payload.CompleteSwingPayload;
 
+import com.kelco.kamenridercraft.world.attribute.AttributeRegistry;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.client.event.InputEvent;
+import net.neoforged.neoforge.event.entity.living.LivingEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
 
 public class ModClientEvents {
@@ -16,6 +26,14 @@ public class ModClientEvents {
         @SubscribeEvent
         public void completeSummonSwing(InputEvent.InteractionKeyMappingTriggered event) {
             if (event.isAttack() && event.shouldSwingHand()) PacketDistributor.sendToServer(new CompleteSwingPayload(event.getHand() == InteractionHand.MAIN_HAND ? 0 : 1));
+        }
+        @SubscribeEvent
+        public void addLivingDamageEvent(LivingEvent.LivingVisibilityEvent event) {
+
+
+            if (event.getLookingEntity() instanceof LivingEntity entity) {
+                //if (entity.getAttribute(AttributeRegistry.IS_TRANSFORMING).getBaseValue()!=0)entity.getAttribute(AttributeRegistry.IS_TRANSFORMING).setBaseValue(entity.getAttribute(AttributeRegistry.IS_TRANSFORMING).getBaseValue()-0.2);
+            }
         }
     }
 }
--- src/main/java/com/kelco/kamenridercraft/events/ModCommonEvents.java
+++ src/main/java/com/kelco/kamenridercraft/events/ModCommonEvents.java
@@ -35,6 +35,7 @@ import com.kelco.kamenridercraft.item.reiwa.gavv.GochipodItem;
 import com.kelco.kamenridercraft.item.showa.*;
 import com.kelco.kamenridercraft.level.ModGameRules;
 import com.kelco.kamenridercraft.network.payload.AbilityKeyPayload;
+import com.kelco.kamenridercraft.network.payload.AttributeChangePayload;
 import com.kelco.kamenridercraft.network.payload.BeltKeyPayload;
 import com.kelco.kamenridercraft.network.payload.PoseKeyPayload;
 import com.kelco.kamenridercraft.particle.ModParticles;
@@ -164,6 +165,7 @@ public class ModCommonEvents {
 
         @SubscribeEvent
         public void addLivingDamageEvent(LivingEvent.LivingVisibilityEvent event) {
+
             if (event.getLookingEntity() instanceof LivingEntity entity) {
                 //if (entity.getAttribute(AttributeRegistry.IS_TRANSFORMING).getBaseValue()!=0)entity.getAttribute(AttributeRegistry.IS_TRANSFORMING).setBaseValue(entity.getAttribute(AttributeRegistry.IS_TRANSFORMING).getBaseValue()-0.2);
             }
@@ -172,7 +174,77 @@ public class ModCommonEvents {
 
         @SubscribeEvent
         public void onEntityTick(EntityTickEvent.Post event) {
+
+
             if (event.getEntity() instanceof LivingEntity player ) {
+
+               if (player.level().isClientSide()) {
+                   float X = 0;
+                   float Y = 0;
+                   float Z = 0;
+                   boolean isPlayer = false;
+                   if (player instanceof Mob mob) {
+                       if (player.getDeltaMovement().x != 0 || player.getDeltaMovement().z != 0) {
+                           X = mob.getViewXRot(1);
+                           Vec3 look = player.getLookAngle();
+                           if (look.x > 0 & player.getDeltaMovement().x > 0) Z = 1;
+                           else if (look.z > 0 & player.getDeltaMovement().z > 0) Z = 1;
+                           else if (look.x < 0 & player.getDeltaMovement().x < 0) Z = 1;
+                           else if (look.z < 0 & player.getDeltaMovement().z < 0) Z = 1;
+                           else Z = -1;
+                       }
+                       player.getAttribute(AttributeRegistry.BALL_ROT_OLD).setBaseValue(player.getAttribute(AttributeRegistry.BALL_ROT).getBaseValue());
+                       player.getAttribute(AttributeRegistry.WHEEL_ROT_OLD).setBaseValue(player.getAttribute(AttributeRegistry.WHEEL_ROT).getBaseValue());
+                       player.getAttribute(AttributeRegistry.CAPE_ROT_OLD).setBaseValue(player.getAttribute(AttributeRegistry.CAPE_ROT).getBaseValue());
+
+                       if (player.getItemBySlot(EquipmentSlot.FEET).getItem() instanceof RiderDriverItem belt) {
+
+                           if (belt.HasCpae(player.getItemBySlot(EquipmentSlot.FEET))) {
+
+                               float cape = (float) player.getAttribute(AttributeRegistry.CAPE_ROT).getBaseValue();
+                               float ball = 0;
+                               if (Z > 0 & cape > -0.7 & !player.isSwimming())
+                                   cape = cape - 0.01f - (player.getSpeed() / 10);
+                               else if (Z > 0 & cape > -0.7 & !player.isSwimming())
+                                   cape = 0;
+                               else if (X == 0 & Z < 0 & cape < 0) cape = cape + 0.2f;
+                               else if (X == 0 & Z == 0 & cape < 0 & X == 0 || X == 0 & Z == 0 & cape < -0.7 || X == 0 & cape < 0 & player.isSwimming())
+                                   cape = cape + 0.02f;
+                               if (X > 0) {
+                                   ball = 0.2f;
+                                   if (isPlayer & Z == 0 & cape > -0.7)
+                                       cape = cape - 0.02f - (player.getSpeed() / 10);
+                               }
+                               if (X < 0) {
+                                   ball = -0.2f;
+                                   if (isPlayer & Z == 0 & cape > -0.7)
+                                       cape = cape - 0.02f - (player.getSpeed() / 10);
+                               }
+                               //if (player.fallDistance > 0 & !player.isSwimming() & cape > -2) cape = cape - 0.05f;
+
+                               player.getAttribute(AttributeRegistry.BALL_ROT).setBaseValue(ball);
+                               player.getAttribute(AttributeRegistry.CAPE_ROT).setBaseValue(cape);
+                           }
+                           if (RiderDriverItem.get_Form_Item(player.getItemBySlot(EquipmentSlot.FEET), 1).get_is_Bike()) {
+                               float wheel = 0;
+                               if (Z > 0) wheel = -0.1f;
+                               if (Z < 0) wheel = 0.1f;
+                               player.getAttribute(AttributeRegistry.WHEEL_ROT).setBaseValue(player.getAttribute(AttributeRegistry.WHEEL_ROT).getBaseValue() + wheel);
+                               float ball = 0;
+                               if (X > 0) {
+                                   ball = 0.5f;
+                                   if (Z == 0) wheel = -0.1f;
+                               }
+                               if (X < 0) {
+                                   ball = -0.5f;
+                                   if (Z == 0) wheel = -0.1f;
+                               }
+                               player.getAttribute(AttributeRegistry.BALL_ROT).setBaseValue(ball);
+                               player.getAttribute(AttributeRegistry.WHEEL_ROT).setBaseValue(player.getAttribute(AttributeRegistry.WHEEL_ROT).getBaseValue() + wheel);
+                           }
+                       }
+                   }
+               }
                 player.getAttribute(AttributeRegistry.IS_TRANSFORMING_OLD).setBaseValue(player.getAttribute(AttributeRegistry.IS_TRANSFORMING).getBaseValue());
                 if (player.getAttribute(AttributeRegistry.IS_TRANSFORMING).getBaseValue()!=0)player.getAttribute(AttributeRegistry.IS_TRANSFORMING).setBaseValue(player.getAttribute(AttributeRegistry.IS_TRANSFORMING).getBaseValue()-0.5);
                 if (player.getAttribute(AttributeRegistry.IS_TRANSFORMING).getBaseValue()<=0)player.getAttribute(AttributeRegistry.IS_TRANSFORMING).setBaseValue(0);
