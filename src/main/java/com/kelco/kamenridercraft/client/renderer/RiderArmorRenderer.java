a002dc6bdb3c59731a805053e48d6c1ad42a6dcc
Blurtle <>
2026-06-17 12:41:42 -0600

    glow attempt

--- src/main/java/com/kelco/kamenridercraft/client/renderer/RiderArmorRenderer.java
+++ src/main/java/com/kelco/kamenridercraft/client/renderer/RiderArmorRenderer.java
@@ -1,6 +1,5 @@
 package com.kelco.kamenridercraft.client.renderer;
 
-import com.kelco.kamenridercraft.KamenRiderCraftCore;
 import com.kelco.kamenridercraft.client.models.RiderArmorModel;
 import com.kelco.kamenridercraft.client.render_layers.RiderRenderLayer;
 import com.kelco.kamenridercraft.effects.EffectCore;
@@ -16,13 +15,13 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.decoration.ArmorStand;
 import org.jetbrains.annotations.Nullable;
-import software.bernie.geckolib.cache.texture.AutoGlowingTexture;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 import software.bernie.geckolib.renderer.layer.GeoRenderLayer;
 import software.bernie.geckolib.util.RenderUtil;
 
 import static com.kelco.kamenridercraft.client.ModRenderTypes.mutekiGlint;
+import static net.minecraft.client.renderer.RenderType.ENTITY_TRANSLUCENT_EMISSIVE;
 import static software.bernie.geckolib.cache.texture.GeoAbstractTexture.appendToPath;
 
 public class RiderArmorRenderer extends GeoArmorRenderer<RiderArmorItem> {
@@ -35,8 +34,8 @@ public class RiderArmorRenderer extends GeoArmorRenderer<RiderArmorItem> {
                     LivingEntity RIDER = renderer2.GetEntity();
                     if (RIDER != null && RIDER.getItemBySlot(EquipmentSlot.FEET).getItem() instanceof RiderDriverItem belt) {
                         ResourceLocation path = appendToPath(model.getTextureResource(animatable,renderer2), "_glowmask");
-                        return belt.getGlowForSlot(RIDER.getItemBySlot(EquipmentSlot.FEET), equipmentSlot, RIDER) ? AutoGlowingTexture.getRenderType(getTextureResource(animatable)) : null;
-                        //return belt.getGlowForSlot(RIDER.getItemBySlot(EquipmentSlot.FEET), renderer2.getCurrentSlot(), RIDER) ? RenderType.entityTranslucentEmissive(path): null;
+                        //return belt.getGlowForSlot(RIDER.getItemBySlot(EquipmentSlot.FEET), equipmentSlot, RIDER) ? AutoGlowingTexture.getRenderType(getTextureResource(animatable)) : null;
+                        return belt.getGlowForSlot(RIDER.getItemBySlot(EquipmentSlot.FEET), renderer2.getCurrentSlot(), RIDER) ? (RenderType)ENTITY_TRANSLUCENT_EMISSIVE.apply(path, false): null;
                     }
                 }return null;
             }
