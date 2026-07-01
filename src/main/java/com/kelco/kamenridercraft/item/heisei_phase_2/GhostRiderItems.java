cd7f87ec85084ad752b732c0b6f7b97f50e7453b
AndrewBean684 <234824004+AndrewBean684@users.noreply.github.com>
2026-07-01 15:56:53 -0400

    i feel like im forgetting someone

--- src/main/java/com/kelco/kamenridercraft/item/heisei_phase_2/GhostRiderItems.java
+++ src/main/java/com/kelco/kamenridercraft/item/heisei_phase_2/GhostRiderItems.java
@@ -799,7 +799,7 @@ public class GhostRiderItems {
 							player.getX(), player.getY()+1,
 							player.getZ(), 70, 0, 0, 0, 1);
 				}}
-			}.changeModel("damashii.geo.json").changeSlot(2).addToList(KamenRiderCraftCore.CreativeTabRegistry.GHOST_TAB_ITEM));
+			}.changeModel("tenkatoitsu_damashii.geo.json").hasCape().changeSlot(2).addToList(KamenRiderCraftCore.CreativeTabRegistry.GHOST_TAB_ITEM));
 
 	public static final DeferredItem<Item> SHINSENGUMI_GHOST_EYECON = ITEMS.register("shinsengumi_ghost_eyecon",
 			() -> new RiderFormChangeItem(new Item.Properties(),"shinsengumi_damashii","ghost","ghostdriver_belt",
