b4c6f4e1e202bed1263971d28adde5a5c9043df9
Blurtle <>
2026-07-06 10:16:00 -0600

    demons

--- src/main/java/com/kelco/kamenridercraft/item/reiwa/revice/DemonsDriverItem.java
+++ src/main/java/com/kelco/kamenridercraft/item/reiwa/revice/DemonsDriverItem.java
@@ -27,8 +27,6 @@ import java.util.Objects;
 import java.util.function.Consumer;
 
 public class DemonsDriverItem extends RiderDriverItem {
-
-
     public DemonsDriverItem(Holder<ArmorMaterial> material, String rider, DeferredItem<Item> baseFormItem, DeferredItem<Item> head, DeferredItem<Item> torso, DeferredItem<Item> legs, Item.Properties properties) {
         super(material, rider, baseFormItem, head, torso, legs, properties);
 
@@ -41,44 +39,29 @@ public class DemonsDriverItem extends RiderDriverItem {
         hasBasicBeltInfo = false;
         tooltipComponents.add(Component.translatable("kamenridercraft.name." + riderName));
 
-        if (getFormItem(stack, 2) == ReviceRiderItems.ANOMALOCARIS_VISTAMP.get() & getFormItem(stack, 5)
-                == ReviceRiderItems.BATTA_VISTAMP.get() & getFormItem(stack, 4)
-                == ReviceRiderItems.SCORPION_VISTAMP.get())
+        if (getFormItem(stack, 2) == ReviceRiderItems.ANOMALOCARIS_VISTAMP.get() & getFormItem(stack, 5) == ReviceRiderItems.BATTA_VISTAMP.get() & getFormItem(stack, 4) == ReviceRiderItems.SCORPION_VISTAMP.get()) {
             tooltipComponents.add(Component.translatable("kamenridercraft:full_genomix.form"));
-
-        else if (getFormItem(stack, 1) == ReviceRiderItems.GIANT_SPIDER_VISTAMP.get())
+        } else if (getFormItem(stack, 1) == ReviceRiderItems.GIANT_SPIDER_VISTAMP.get()) {
             tooltipComponents.add(Component.translatable("kamenridercraft:giant_genomix.form"));
-
-        else {
+        } else {
             tooltipComponents.add(Component.translatable("kamenridercraft:genomix.form"));
 
-            if (getFormItem(stack, 2) == ReviceRiderItems.ANOMALOCARIS_VISTAMP.get())
-                tooltipComponents.add(Component.translatable("kamenridercraft:anomalocaris_vistamp.form"));
-            if (getFormItem(stack, 2) == ReviceRiderItems.KOMODO_DRAGON_VISTAMP.get())
-                tooltipComponents.add(Component.translatable("kamenridercraft:komodo_dragon_vistamp.form"));
-            if (getFormItem(stack, 2) == ReviceRiderItems.KOMODO_DRAGON_VISTAMP_DEMONS.get())
-                tooltipComponents.add(Component.translatable("kamenridercraft:komodo_dragon_vistamp.form"));
-            if (getFormItem(stack, 2) == ReviceRiderItems.MOGURA_VISTAMP.get())
-                tooltipComponents.add(Component.translatable("kamenridercraft:mogura_vistamp.form"));
-            if (getFormItem(stack, 3) == ReviceRiderItems.BATTA_VISTAMP.get())
-                tooltipComponents.add(Component.translatable("kamenridercraft:batta_vistamp.form"));
-            if (getFormItem(stack, 3) == ReviceRiderItems.CONDOR_VISTAMP_DEMONS.get())
-                tooltipComponents.add(Component.translatable("kamenridercraft:condor_vistamp_demons.form"));
-            if (getFormItem(stack, 3) == ReviceRiderItems.CROCODILE_VISTAMP.get())
-                tooltipComponents.add(Component.translatable("kamenridercraft:crocodile_vistamp.form"));
-            if (getFormItem(stack, 3) == ReviceRiderItems.CROCODILE_VISTAMP_DEMONS.get())
-                tooltipComponents.add(Component.translatable("kamenridercraft:crocodile_vistamp_demons.form"));
-            if (getFormItem(stack, 4) == ReviceRiderItems.SCORPION_VISTAMP.get())
-                tooltipComponents.add(Component.translatable("kamenridercraft:scorpion_vistamp.form"));
-            if (getFormItem(stack, 5) == ReviceRiderItems.BATTA_VISTAMP.get())
-                tooltipComponents.add(Component.translatable("kamenridercraft:batta_vistamp.form"));
+            for (int n = 2; n <= 5; n++) {
+                if (!getFormItem(stack, n).toString().contains("blank")) {
+                    if (getFormItem(stack, n) == ReviceRiderItems.CONDOR_VISTAMP_DEMONS.get()) {
+                        tooltipComponents.add(Component.translatable("kamenridercraft:condor_vistamp_demons.form"));
+                    } else {
+                        tooltipComponents.add(Component.translatable(getFormItem(stack, n).toString() + ".form"));
+                    }
+                }
+            }
         }
         super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 
     @Override
     public String getText(ItemStack itemstack, EquipmentSlot equipmentSlot, LivingEntity rider, String riderName) {
-        boolean fly = rider.getAttribute(Attributes.WINGS_OUT).getBaseValue() == 1;
+        boolean fly = Objects.requireNonNull(rider.getAttribute(Attributes.WINGS_OUT)).getBaseValue() == 1;
         switch (equipmentSlot) {
             case EquipmentSlot.FEET:
                 String belt = ((RiderDriverItem) itemstack.getItem()).beltText;
@@ -93,33 +76,22 @@ public class DemonsDriverItem extends RiderDriverItem {
             default:
                 return riderName + getFormItem(itemstack, 1).getFormName(fly);
         }
-
     }
 
     public boolean getGlowForSlot(ItemStack itemstack, EquipmentSlot currentSlot, LivingEntity livingEntity) {
-
         if (currentSlot == EquipmentSlot.FEET) {
             return getFormItem(itemstack, 1).getIsBeltGlowing();
         }
-        if (isTransformed(livingEntity)) {
-            switch (currentSlot) {
-                case LEGS, CHEST -> {
-                    return false;
-                }
-                case HEAD -> {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return isTransformed(livingEntity) && currentSlot == EquipmentSlot.HEAD;
     }
 
     public ResourceLocation getModelResource(ItemStack itemstack, RiderArmorItem animatable, EquipmentSlot slot, LivingEntity rider) {
         int num = 1;
-        if (slot == EquipmentSlot.CHEST || slot == EquipmentSlot.LEGS)
+        if (slot == EquipmentSlot.CHEST || slot == EquipmentSlot.LEGS) {
             return ResourceLocation.fromNamespaceAndPath(KamenRiderCraftCore.MOD_ID, "geo/default_wings_armor.geo.json");
+        }
 
-        if (getFormItem(itemstack, num).hasWingsIfFlying() && rider.getAttribute(Attributes.WINGS_OUT).getBaseValue() == 1) {
+        if (getFormItem(itemstack, num).hasWingsIfFlying() && Objects.requireNonNull(rider.getAttribute(Attributes.WINGS_OUT)).getBaseValue() == 1) {
             return ResourceLocation.fromNamespaceAndPath(KamenRiderCraftCore.MOD_ID, "geo/" + getFormItem(itemstack, num).getFlyingModel(this.riderName));
         }
         return ResourceLocation.fromNamespaceAndPath(KamenRiderCraftCore.MOD_ID, "geo/" + getFormItem(itemstack, num).getModel(this.riderName));
@@ -127,43 +99,37 @@ public class DemonsDriverItem extends RiderDriverItem {
     }
 
     public boolean getPartsForSlot(ItemStack itemstack, EquipmentSlot currentSlot, String part) {
-
         switch (currentSlot) {
             case HEAD -> {
-                if (Objects.equals(part, "head")) return true;
-                if (Objects.equals(part, "body")) return true;
-                if (Objects.equals(part, "rightArm")) return true;
-                if (Objects.equals(part, "leftArm")) return true;
-                if (Objects.equals(part, "rightLeg")) return true;
-                if (Objects.equals(part, "leftLeg")) return true;
+                return true;
             }
             case CHEST -> {
-                if (Objects.equals(part, "body"))
+                if (part.equals("body")) {
                     return getFormItem(itemstack, 4) == ReviceRiderItems.SCORPION_VISTAMP.get();
+                }
 
                 if (getFormItem(itemstack, 1) == ReviceRiderItems.GIANT_SPIDER_VISTAMP.get()) {
-                    if (Objects.equals(part, "rightArm"))
+                    if (part.equals("rightArm")) {
                         return getFormItem(itemstack, 2) == ReviceRiderItems.KOMODO_DRAGON_VISTAMP_DEMONS.get();
-                    if (Objects.equals(part, "leftArm"))
+                    } else if (part.equals("leftArm")) {
                         return getFormItem(itemstack, 3) == ReviceRiderItems.CROCODILE_VISTAMP_DEMONS.get();
+                    }
 
                 } else {
-                    if (Objects.equals(part, "rightArm"))
+                    if (part.equals("rightArm") || part.equals("leftArm")) {
                         return getFormItem(itemstack, 2) == ReviceRiderItems.ANOMALOCARIS_VISTAMP.get();
-                    if (Objects.equals(part, "leftArm"))
-                        return getFormItem(itemstack, 2) == ReviceRiderItems.ANOMALOCARIS_VISTAMP.get();
+                    }
                 }
-                if (Objects.equals(part, "rightLeg"))
+                if (part.equals("rightLeg") || part.equals("leftLeg")) {
                     return getFormItem(itemstack, 5) == ReviceRiderItems.BATTA_VISTAMP.get();
-                if (Objects.equals(part, "leftLeg"))
-                    return getFormItem(itemstack, 5) == ReviceRiderItems.BATTA_VISTAMP.get();
+                }
             }
             case LEGS -> {
-                if (Objects.equals(part, "body"))
+                if (part.equals("body")) {
                     return getFormItem(itemstack, 3) == ReviceRiderItems.CONDOR_VISTAMP_DEMONS.get();
-
-                if (Objects.equals(part, "rightArm"))
+                } else if (part.equals("rightArm")) {
                     return getFormItem(itemstack, 2) == ReviceRiderItems.MOGURA_VISTAMP.get();
+                }
             }
         }
         return false;
@@ -173,10 +139,10 @@ public class DemonsDriverItem extends RiderDriverItem {
     public void setExtraFormItem(ItemStack belt, Item ITEM, int SLOT, CompoundTag tag) {
         if (((RiderFormChangeItem) ITEM).getSlot() == 1 & ModdedItemCore.BLANK_FORM.get() != ITEM) {
             Consumer<CompoundTag> data = form -> {
-                for (int n = 2; n < 6; n++)
+                for (int n = 2; n <= 5; n++) {
                     form.putString("slot_tex" + n, (ModdedItemCore.BLANK_FORM.get()).toString());
+                }
             };
-
             CustomData.update(DataComponents.CUSTOM_DATA, belt, data);
         }
     }
