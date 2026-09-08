fcd7919ae8776cbb9a830ed8c2aed70e13f4fa25
Blurtle <>
2026-09-07 18:30:08 -0600

    Somnia + henchman change

--- src/main/java/com/kelco/kamenridercraft/entity/mobs/foot_soldiers/BaseHenchmenEntity.java
+++ src/main/java/com/kelco/kamenridercraft/entity/mobs/foot_soldiers/BaseHenchmenEntity.java
@@ -172,17 +172,15 @@ public abstract class BaseHenchmenEntity extends Monster implements RangedAttack
                 int tempRandZ = Mth.floor(this.getZ()) + Mth.nextInt(this.random, 9, 35) * Mth.nextInt(this.random, -1, 1);
                 reinforcement.setPos(tempRandX, this.getY(), tempRandZ);
 
-                for (int i = 0; i < 100; ++i){
+                for (int i = 0; i < 100; ++i) {
                     int randX = Mth.floor(this.getX()) + Mth.nextInt(this.random, 9, 35) * Mth.nextInt(this.random, -1, 1);
                     int randY = Mth.floor(this.getY()) + Mth.nextInt(this.random, 9, 35) * Mth.nextInt(this.random, -1, 1);
                     int randZ = Mth.floor(this.getZ()) + Mth.nextInt(this.random, 9, 35) * Mth.nextInt(this.random, -1, 1);
 
                     BlockPos potentialPosition = new BlockPos(randX, randY, randZ);
-                    if (!SpawnPlacements.isSpawnPositionOk(this.getType(), serverLevel, potentialPosition)) {
+                    if (SpawnPlacements.isSpawnPositionOk(this.getType(), serverLevel, potentialPosition)) {
                         reinforcement.setPos(randX, this.getY(), randZ);
-                        continue;
                     }
-                    reinforcement.setPos(tempRandX, this.getY(), tempRandZ);
                 }
 
                 reinforcement.getAttribute(Attributes.REINFORCEMENT_CHANCE).setBaseValue(0);
