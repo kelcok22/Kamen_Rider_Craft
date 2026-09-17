c106cf72236de4cec25e2b2f7f29bd8a4f9aae85
Blurtle <>
2026-09-17 06:21:02 -0600

    Undead changes

--- src/main/java/com/kelco/kamenridercraft/entity/mobs/foot_soldiers/UndeadEntity.java
+++ src/main/java/com/kelco/kamenridercraft/entity/mobs/foot_soldiers/UndeadEntity.java
@@ -1,13 +1,21 @@
 package com.kelco.kamenridercraft.entity.mobs.foot_soldiers;
 
+import com.kelco.kamenridercraft.item.base_items.RiderDriverItem;
 import com.kelco.kamenridercraft.item.heisei_phase_1.BladeRiderItems;
 import com.kelco.kamenridercraft.world.attribute.Attributes;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
+
+import javax.annotation.Nullable;
 
 public class UndeadEntity extends BaseHenchmenEntity {
     public UndeadEntity(EntityType<? extends BaseHenchmenEntity> type, Level level) {
@@ -18,6 +26,55 @@ public class UndeadEntity extends BaseHenchmenEntity {
         setItemSlot(EquipmentSlot.CHEST, new ItemStack(BladeRiderItems.BLADECHESTPLATE.get()));
         setItemSlot(EquipmentSlot.LEGS, new ItemStack(BladeRiderItems.BLADELEGGINGS.get()));
         setItemSlot(EquipmentSlot.FEET, new ItemStack(BladeRiderItems.UNDEAD_BUCKLE.get()));
+    }
+
+    @Nullable
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor serverLevelAccessor, DifficultyInstance difficultyInstance, 
+                                        MobSpawnType mobSpawnType, @Nullable SpawnGroupData spawnGroupData) {
+        spawnGroupData = super.finalizeSpawn(serverLevelAccessor, difficultyInstance, mobSpawnType, spawnGroupData);
+        RiderDriverItem.setUpdateForm(getItemBySlot(EquipmentSlot.FEET));
+
+        switch (serverLevelAccessor.getRandom().nextInt(9)) {
+            case 0:
+                setItemSlot(EquipmentSlot.FEET, new ItemStack(BladeRiderItems.UNDEAD_BUCKLE.get()));
+                RiderDriverItem.setFormItem(getItemBySlot(EquipmentSlot.FEET), BladeRiderItems.KICK_LOCUST.get(), 1);
+                break;
+            case 1:
+                setItemSlot(EquipmentSlot.FEET, new ItemStack(BladeRiderItems.UNDEAD_BUCKLE.get()));
+                RiderDriverItem.setFormItem(getItemBySlot(EquipmentSlot.FEET), BladeRiderItems.THUNDER_DEER.get(), 1);
+                break;
+            case 2:
+                setItemSlot(EquipmentSlot.FEET, new ItemStack(BladeRiderItems.UNDEAD_BUCKLE.get()));
+                RiderDriverItem.setFormItem(getItemBySlot(EquipmentSlot.FEET), BladeRiderItems.MACH_JAGUAR.get(), 1);
+                setItemInHand(InteractionHand.MAIN_HAND, new ItemStack(BladeRiderItems.JAGUAR_CLAWS.get(), 1));
+                break;
+            case 3:
+                setItemSlot(EquipmentSlot.FEET, new ItemStack(BladeRiderItems.UNDEAD_BUCKLE.get()));
+                RiderDriverItem.setFormItem(getItemBySlot(EquipmentSlot.FEET), BladeRiderItems.RAPID_PECKER.get(), 1);
+                break;
+            case 4:
+                setItemSlot(EquipmentSlot.FEET, new ItemStack(BladeRiderItems.UNDEAD_BUCKLE.get()));
+                RiderDriverItem.setFormItem(getItemBySlot(EquipmentSlot.FEET), BladeRiderItems.ROCK_TORTOISE.get(), 1);
+                break;
+            case 5:
+                setItemSlot(EquipmentSlot.FEET, new ItemStack(BladeRiderItems.UNDEAD_BUCKLE.get()));
+                RiderDriverItem.setFormItem(getItemBySlot(EquipmentSlot.FEET), BladeRiderItems.SCOPE_BAT.get(), 1);
+                break;
+            case 6:
+                setItemSlot(EquipmentSlot.FEET, new ItemStack(BladeRiderItems.UNDEAD_BUCKLE.get()));
+                RiderDriverItem.setFormItem(getItemBySlot(EquipmentSlot.FEET), BladeRiderItems.REFLECT_MOTH.get(), 1);
+                break;
+            case 7:
+                setItemSlot(EquipmentSlot.FEET, new ItemStack(BladeRiderItems.UNDEAD_BUCKLE.get()));
+                RiderDriverItem.setFormItem(getItemBySlot(EquipmentSlot.FEET), BladeRiderItems.SHUFFLE_CENTIPEDE.get(), 1);
+                break;
+            case 8:
+                setItemSlot(EquipmentSlot.FEET, new ItemStack(BladeRiderItems.UNDEAD_BUCKLE.get()));
+                RiderDriverItem.setFormItem(getItemBySlot(EquipmentSlot.FEET), BladeRiderItems.FLOAT_DRAGONFLY.get(), 1);
+                setItemInHand(InteractionHand.MAIN_HAND, new ItemStack(BladeRiderItems.DRAGONFLY_SOMERSAULT.get(), 1));
+                break;
+        }
+        return spawnGroupData;
     }
 
     public static AttributeSupplier.Builder setAttributes() {
