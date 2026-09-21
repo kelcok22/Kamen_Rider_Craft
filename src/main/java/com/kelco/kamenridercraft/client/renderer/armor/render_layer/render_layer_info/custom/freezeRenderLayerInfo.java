7859150bdef56dc947d54c41b95b5a12916a98a7
kelco <12749084+kelcok22@users.noreply.github.com>
2026-09-21 18:22:28 +0100

    freeze edit
    Signed-off-by: kelco <12749084+kelcok22@users.noreply.github.com>

--- src/main/java/com/kelco/kamenridercraft/client/renderer/armor/render_layer/render_layer_info/custom/freezeRenderLayerInfo.java
+++ src/main/java/com/kelco/kamenridercraft/client/renderer/armor/render_layer/render_layer_info/custom/freezeRenderLayerInfo.java
@@ -1,13 +1,13 @@
 package com.kelco.kamenridercraft.client.renderer.armor.render_layer.render_layer_info.custom;
 
 import com.kelco.kamenridercraft.client.renderer.armor.render_layer.render_layer_info.RenderLayerInfo;
-import com.kelco.kamenridercraft.world.attribute.Attributes;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.cache.object.GeoBone;
-
-import java.util.Objects;
 
 
 public class freezeRenderLayerInfo extends RenderLayerInfo {
@@ -18,8 +18,12 @@ public class freezeRenderLayerInfo extends RenderLayerInfo {
     public void ApplyMovement(BakedGeoModel model, ItemStack stack, LivingEntity entity, float partialTick) {
         GeoBone door = model.getBone("door").orElse(null);
         if (door != null) {
-            if(Objects.requireNonNull(entity.getAttribute(Attributes.WINGS_OUT)).getValue()!=0) door.setRotY(-2);
+            if(isByWater(entity))door.setRotY(-2);
             else door.setRotY(0);
         }
+    }
+    boolean isByWater(LivingEntity entity){
+        if (entity.level().getBlockState(new BlockPos((int)entity.getX(),(int)entity.getY()-1,(int)entity.getZ())).is(Blocks.ICE))return true;
+        return false;
     }
 }
